75df230e8273ea1fe593370a51439600772fc3f8
Peter Jankuliak <p.jankuliak@gmail.com>
2020-10-05 10:21:09 +0200

    Modify hex functions in src/hex.h
    Mainly to work with static arrays insted of dynamic strings

--- src/hex.h
+++ src/hex.h
@@ -1,5 +1,8 @@
+#pragma once
+
 #include <string>
 #include <array>
+#include "block_id.h"
 
 namespace ouisync {
 
@@ -24,15 +27,16 @@ inline bool is_hex(boost::string_view s)
     return true;
 }
 
-template<class T, size_t N> std::array<T, 2*N> to_hex(const std::array<T, N>& as)
+template<class OutputT, size_t N, class InputT> std::array<OutputT, 2*N> to_hex(const std::array<InputT, N>& as)
 {
-    static_assert(is_byte_type<T>::value, "Not a bytestring type");
+    static_assert(is_byte_type<InputT>::value, "Not a bytestring type");
 
-    std::array<T, 2*N> output;
+    std::array<OutputT, 2*N> output;
+
+    const char* digits = "0123456789abcdef";
 
     for (unsigned int i = 0; i < as.size(); i++) {
         unsigned char c = as.data()[i];
-        const char* digits = "0123456789abcdef";
         output[2*i]     = digits[(c >> 4) & 0xf];
         output[2*i + 1] = digits[(c >> 0) & 0xf];
     }
@@ -40,16 +44,22 @@ template<class T, size_t N> std::array<T, 2*N> to_hex(const std::array<T, N>& as
     return output;
 }
 
-template<class S> std::string to_hex(const S& bytestring)
+template<class OutputT, size_t InputSize, class InputT>
+inline
+std::array<OutputT, InputSize*2> to_hex(const InputT* input)
 {
-    static_assert(is_byte_type<decltype(*bytestring.begin())>::value, "Not a bytestring type");
-    std::string output;
-    for (unsigned int i = 0; i < bytestring.size(); i++) {
-        unsigned char c = bytestring.data()[i];
-        const char* digits = "0123456789abcdef";
-        output += digits[(c >> 4) & 0xf];
-        output += digits[(c >> 0) & 0xf];
+    static_assert(is_byte_type<InputT>::value, "Not a bytestring type");
+
+    std::array<OutputT, InputSize*2> output;
+
+    const char* digits = "0123456789abcdef";
+
+    for (unsigned int i = 0; i < InputSize; i++) {
+        unsigned char c = reinterpret_cast<const unsigned char*>(input)[i];
+        output[2*i]   = digits[(c >> 4) & 0xf];
+        output[2*i+1] = digits[(c >> 0) & 0xf];
     }
+
     return output;
 }
 
@@ -78,6 +88,30 @@ boost::optional<unsigned char> from_hex(char c1, char c2)
 inline boost::optional<std::string> from_hex(boost::string_view hex)
 {
     std::string output((hex.size() >> 1) + (hex.size() & 1), '\0');
+
+    size_t i = 0;
+    while (size_t s = hex.size()) {
+        boost::optional<unsigned char> oc;
+
+        if (s == 1) { oc = from_hex(hex[0]);         hex.remove_prefix(1); }
+        else        { oc = from_hex(hex[0], hex[1]); hex.remove_prefix(2); }
+
+        if (!oc) return boost::none;
+
+        output[i++] = *oc;
+    }
+
+    return output;
+}
+
+template<class OutputT, size_t InputSize>
+inline boost::optional<std::array<OutputT, InputSize/2>> from_hex(boost::string_view hex)
+{
+    static_assert(InputSize % 2 == 0, "");
+
+    if (InputSize != hex.size()) return boost::none;
+
+    std::array<OutputT, InputSize/2> output;
 
     size_t i = 0;
     while (size_t s = hex.size()) {
