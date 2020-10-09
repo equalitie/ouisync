588a0cbb5393eebe2842b13505847aad36794d84
Peter Jankuliak <p.jankuliak@gmail.com>
2020-10-09 10:35:16 +0200

    Move function from tag.h to tag.cpp

---
+++ src/object/tag.cpp
@@ -0,0 +1,12 @@
+#include <ostream>
+#include "tag.h"
+
+using namespace ouisync::object;
+
+std::ostream& ouisync::object::operator<<(std::ostream& os, Tag tag) {
+    switch (tag) {
+        case Tag::Tree:  return os << "Tree";
+        case Tag::Block: return os << "Block";
+    }
+    return os << "Unknown";
+}
--- src/object/tag.h
+++ src/object/tag.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iosfwd>
+
 namespace ouisync::object {
 
 enum class Tag : uint8_t {
@@ -12,5 +14,7 @@ class Block;
 template<class> struct GetTag;
 template<> struct GetTag<Tree>  { static constexpr Tag value = Tag::Tree; };
 template<> struct GetTag<Block> { static constexpr Tag value = Tag::Block; };
+
+std::ostream& operator<<(std::ostream& os, Tag tag);
 
 } // namespaces
