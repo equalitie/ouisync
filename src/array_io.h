e006ad733a9de2a463e771fcb78f5a77ede9aa9f
Peter Jankuliak <p.jankuliak@gmail.com>
2020-10-09 10:36:04 +0200

    Pragma once and inline function in array_io.h

--- src/array_io.h
+++ src/array_io.h
@@ -1,9 +1,11 @@
+#pragma once
+
 #include <iostream>
 #include <array>
 
 namespace std {
-
     template<class T, size_t N>
+    inline
     std::ostream& operator<<(std::ostream& os, const std::array<T, N>& as)
     {
         for (auto a : as) {
