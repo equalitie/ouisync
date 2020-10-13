de1cfd794ab8997bf67ffa7ea2871c5ebca89bcc
Peter Jankuliak <p.jankuliak@gmail.com>
2020-10-13 09:41:10 +0200

    Add struct JustTag for loading "just tag"
    Useful for probing whether a file is of certain object type without
    having to parse the entire content.

--- src/object/tag.h
+++ src/object/tag.h
@@ -13,6 +13,21 @@ enum class Tag : std::uint8_t {
     Id
 };
 
+/*
+ * Use JustTag class template to probe whether the given object is of
+ * expected_tag while avoiding reading the object from a file.
+ * E.g.:
+ *
+ * if (maybe_load<JustTag<Block>>(objdir, object_id)) {
+ *     remove(objdir, object_id);
+ * }
+ */
+template<class T>
+struct JustTag {
+    template<class Archive>
+    void serialize(Archive&, const unsigned int version) {}
+};
+
 class Tree;
 class Block;
 
@@ -20,6 +35,7 @@ template<class> struct GetTag;
 template<> struct GetTag<Tree>  { static constexpr Tag value = Tag::Tree;  };
 template<> struct GetTag<Block> { static constexpr Tag value = Tag::Block; };
 template<> struct GetTag<Id>    { static constexpr Tag value = Tag::Id;    };
+template<class T> struct GetTag<JustTag<T>> { static constexpr Tag value = GetTag<T>::value; };
 
 std::ostream& operator<<(std::ostream& os, Tag tag);
 
