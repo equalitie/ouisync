4e6d4dcc7a44f2328d3ef8bc748db60bb2174ca1
Peter Jankuliak <p.jankuliak@gmail.com>
2020-10-14 14:10:27 +0200

    Modify test/cryfs.cpp to test storing and loading

--- test/cryfs.cpp
+++ test/cryfs.cpp
@@ -3,17 +3,23 @@
 
 #include <block_store.h>
 #include <create_cry_device.h>
-#include <version_vector.h>
 
 #include <fspp/fs_interface/Device.h>
+#include <fspp/fs_interface/File.h>
 #include <fspp/fs_interface/Dir.h>
+#include <fspp/fs_interface/OpenFile.h>
 
 using namespace ouisync;
 namespace sys = boost::system;
 using namespace std;
 
-struct TestDevice {
+template<class T> using Ref = cpputils::unique_ref<T>;
 
+using Dir      = fspp::Dir;
+using File     = fspp::File;
+using OpenFile = fspp::OpenFile;
+
+struct TestDevice {
     void ls(const fs::path& dir_path) {
         auto dir = _dev->LoadDir(dir_path);
         assert(dir);
@@ -23,12 +29,7 @@ struct TestDevice {
         }
     }
 
-    void mkdir(const fs::path& dir_path) {
-        auto is_in = [] (const auto& what, const auto& where) {
-            for (auto& w : where) if (w.name == what) return true;
-            return false;
-        };
-
+    Ref<Dir> mkdir(const fs::path& dir_path) {
         fs::path p = "/";
         auto d = _dev->LoadDir(p);
         auto chs = (*d)->children();
@@ -39,16 +40,46 @@ struct TestDevice {
             if (is_in(f, chs)) {
                 d = _dev->LoadDir(p);
             } else {
-                (*d)->createDir(f.string(), fspp::mode_t(0),fspp::uid_t(0),fspp::gid_t(0));
+                (*d)->createDir(f.string(),
+                        fspp::mode_t(0), fspp::uid_t(0), fspp::gid_t(0));
                 d = _dev->LoadDir(p);
             }
             assert(d);
             chs = (*d)->children();
         }
+        return std::move(*d);
     }
+
+    Ref<OpenFile> mkfile(const fs::path& path) {
+        auto dir = mkdir(path.parent_path());
+        return dir->createAndOpenFile(path.filename().native(),
+                fspp::mode_t(0), fspp::uid_t(0), fspp::gid_t(0));
+    }
+
+    Ref<OpenFile> open_file(const fs::path& path) {
+        Opt<Ref<File>> file = _dev->LoadFile(path);
+        assert(file);
+        return (*file)->open(fspp::openflags_t::RDONLY());
+    }
+
+    bool is_in(const auto& what, const auto& where) const {
+        for (auto& w : where) if (w.name == what) return true;
+        return false;
+    };
 
     unique_ptr<fspp::Device> _dev;
 };
+
+void write(Ref<OpenFile>& file, const std::vector<char>& v) {
+    file->write(v.data(), fspp::num_bytes_t(v.size()), fspp::num_bytes_t(0));
+}
+
+std::vector<char> read(Ref<OpenFile>& file, size_t size) {
+    std::vector<char> v(size);
+    auto s = file->read(v.data(), fspp::num_bytes_t(size), fspp::num_bytes_t(0));
+    v.resize(s.value());
+    return v;
+}
 
 int main() {
     fs::path testdir = fs::unique_path("/tmp/ouisync/test-cryfs-%%%%-%%%%-%%%%-%%%%");
@@ -58,10 +89,17 @@ int main() {
         auto cry_device = ouisync::create_cry_device(testdir, true);
         TestDevice device{move(cry_device)};
 
-        device.mkdir("/a/b");
-        device.ls("/");
-        device.ls("/a");
-        sleep(2);
+        auto f = device.mkfile("/a/b");
+        write(f, {1,2,3});
+        f->flush();
+    }
+
+    {
+        auto cry_device = ouisync::create_cry_device(testdir, true);
+        TestDevice device{move(cry_device)};
+        auto f = device.open_file("/a/b");
+        auto v = read(f, 3);
+        assert(v == std::vector<char>({1,2,3}));
     }
 
     //cout << "Deleting " << testdir << "\n";
