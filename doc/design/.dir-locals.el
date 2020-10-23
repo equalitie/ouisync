fd2c838ecb05472f2dd6041c63cf953c3462728c
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2020-10-23 12:39:37 +0200

    Remove directory local variables file.
    The only setting not being a preference of mine was that the document is in
    American English, which does not justify the presence of the whole file.

--- doc/design/.dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((markdown-mode . ((ispell-local-dictionary . "american")
-                   (eval . (visual-fill-column-mode))
-                   (eval . (visual-line-mode))
-                   (eval . (flyspell-mode)))))
