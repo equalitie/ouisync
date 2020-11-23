f6bd82e7760c41a1fbd21d0a84fe63bd8f86d3b1
Peter Jankuliak <p.jankuliak@gmail.com>
2020-11-23 14:12:12 +0100

    Add object::io::exists function

--- src/object/io.cpp
+++ src/object/io.cpp
@@ -11,4 +11,8 @@ bool remove(const fs::path& objdir, const Id& id) {
     return !ec;
 }
 
+bool exists(const fs::path& objdir, const Id& id) {
+    return fs::exists(objdir/path::from_id(id));
+}
+
 } // namespace
