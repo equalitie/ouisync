d3e1ab9df86b485942ede2b4981c826652fff0fc
Peter Jankuliak <p.jankuliak@gmail.com>
2020-12-07 14:05:46 +0100

    Using unofficial CMakeLists.txt for CryptoPP

--- cmake/buildcryptopp-config.cmake
+++ cmake/buildcryptopp-config.cmake
@@ -2,39 +2,11 @@ project(BuildCryptoPP VERSION 1.0.0 LANGUAGES CXX)
 
 include(FetchContent)
 fetchcontent_populate(CryptoPP URL https://github.com/weidai11/cryptopp/archive/CRYPTOPP_8_2_0.tar.gz)
+fetchcontent_populate(CryptoPPCMake URL https://github.com/noloader/cryptopp-cmake/archive/CRYPTOPP_8_2_0.tar.gz)
 
+set(cryptopp_cmake_srcdir ${CMAKE_CURRENT_BINARY_DIR}/cryptoppcmake-src)
 set(cryptopp_srcdir ${CMAKE_CURRENT_BINARY_DIR}/cryptopp-src)
-set(cryptopp_lib ${CMAKE_CURRENT_BINARY_DIR}/cryptopp-src/libcryptopp.a)
-set(cryptopp_flags "CXX=${CMAKE_CXX_COMPILER}")
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    set(cryptopp_flags "${cryptopp_flags} -stdlib=libc++")
-else()
-    set(cryptopp_flags "${cryptopp_flags}")
-endif()
+file(COPY ${cryptopp_cmake_srcdir}/CMakeLists.txt DESTINATION ${cryptopp_srcdir})
 
-if(CMAKE_GENERATOR MATCHES "Makefiles")
-    set(MAKE_CMD $(MAKE))
-else()
-    set(MAKE_CMD make)
-endif()
-
-# XXX: Using both add_custom_command and add_custom_target seem to be faster
-# than just running $(MAKE) directly from add_custom_command. Not sure why.
-add_custom_command(
-    OUTPUT ${cryptopp_lib}
-    COMMAND ${MAKE_CMD} libcryptopp.a ${cryptopp_flags}
-    WORKING_DIRECTORY ${cryptopp_srcdir})
-
-add_custom_target(CryptoPP_Target DEPENDS ${cryptopp_lib})
-
-#add_custom_target(CryptoPP_Target
-#    $(MAKE) libcryptopp.a ${cryptopp_flags}
-#    WORKING_DIRECTORY ${cryptopp_srcdir})
-
-add_library(CryptoPP STATIC IMPORTED)
-add_dependencies(CryptoPP CryptoPP_Target)
-set_target_properties(CryptoPP
-    PROPERTIES
-    IMPORTED_LOCATION ${cryptopp_lib}
-    INTERFACE_INCLUDE_DIRECTORIES ${cryptopp_srcdir})
+add_subdirectory(${cryptopp_srcdir})
