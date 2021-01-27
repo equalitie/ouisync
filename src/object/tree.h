c1ff31d1dec0208cd67dcf6e69063ed6ab8b6afb
Peter Jankuliak <p.jankuliak@gmail.com>
2021-01-27 14:47:20 +0100

    Minor Tree refactor

--- src/object/tree.h
+++ src/object/tree.h
@@ -16,100 +16,103 @@ namespace ouisync::object {
 
 class Tree final {
 private:
-    using Impl = std::map<std::string, ObjectId>;
+    //using NameMap = std::map<std::string, std::map<ObjectId, std::set<VersionVector>>>;
+    using NameMap = std::map<std::string, ObjectId>;
 
     template<bool is_mutable>
     class Handle {
-        using ImplPtr = std::conditional_t<is_mutable, Impl*, const Impl*>;
-        using Iterator = std::conditional_t<is_mutable, Impl::iterator, Impl::const_iterator>;
+        using ImplPtr = std::conditional_t<is_mutable, NameMap*, const NameMap*>;
+        using Iterator = std::conditional_t<is_mutable, NameMap::iterator, NameMap::const_iterator>;
 
       public:
         Handle() = default;
         Handle(const Handle&) = default;
         Handle& operator=(const Handle&) = default;
 
-        Handle(Handle&& other) : impl(other.impl), i(other.i)
+        Handle(Handle&& other) : name_map(other.name_map), i(other.i)
         {
-            other.impl = nullptr;
-            other.i = Impl::iterator();
+            other.name_map = nullptr;
+            other.i = NameMap::iterator();
         }
 
         Handle& operator=(Handle&& other)
         {
-            impl = other.impl;
+            name_map = other.name_map;
             i = other.i;
-            other.impl = nullptr;
-            other.i = Impl::iterator();
+            other.name_map = nullptr;
+            other.i = NameMap::iterator();
             return *this;
         }
 
         ObjectId id() const {
-            assert(impl);
-            if (!impl) exit(1);
+            assert(name_map);
+            if (!name_map) exit(1);
             return i->second;
         }
 
-        operator bool() const { return impl != nullptr; }
+        operator bool() const { return name_map != nullptr; }
 
         void set_id(const ObjectId& id) {
-            assert(impl);
-            if (!impl) exit(1);
+            assert(name_map);
+            if (!name_map) exit(1);
             if (id == i->second) return;
             i->second = id;
         }
 
       private:
         friend class Tree;
-        Handle(ImplPtr impl, Iterator i)
-            : impl(impl), i(i) {}
+        Handle(ImplPtr name_map, Iterator i)
+            : name_map(name_map), i(i) {}
 
       private:
-        ImplPtr impl = nullptr;
+        ImplPtr name_map = nullptr;
         Iterator i;
     };
 
 public:
-    auto begin() const { return _impl.begin(); }
-    auto end()   const { return _impl.end();   }
+    auto begin() const { return _name_map.begin(); }
+    auto end()   const { return _name_map.end();   }
 
-    size_t size() const { return _impl.size(); }
+    size_t size() const { return _name_map.size(); }
 
     using MutableHandle   = Handle<true>;
     using ImmutableHandle = Handle<false>;
 
     ImmutableHandle find(const std::string& k) const {
-        auto i = _impl.find(k);
-        if (i == _impl.end()) return {};
-        return {&_impl, i};
+        auto i = _name_map.find(k);
+        if (i == _name_map.end()) return {};
+        return {&_name_map, i};
     }
 
     MutableHandle find(const std::string& k) {
-        auto i = _impl.find(k);
-        if (i == _impl.end()) return {};
-        return {&_impl, i};
+        auto i = _name_map.find(k);
+        if (i == _name_map.end()) return {};
+        return {&_name_map, i};
     }
 
     void erase(const MutableHandle& h)
     {
-        assert(h.impl);
-        assert(h.impl == &_impl);
-        if (h.impl != &_impl) exit(1);
-        _impl.erase(h.i);
+        assert(h.name_map);
+        assert(h.name_map == &_name_map);
+        if (h.name_map != &_name_map) exit(1);
+        _name_map.erase(h.i);
     }
 
     std::pair<MutableHandle, bool>
     insert(std::pair<std::string, ObjectId> pair) {
-        auto [i,inserted] = _impl.insert(std::move(pair));
-        return {MutableHandle{&_impl, i}, inserted};
+        auto [i,inserted] = _name_map.insert(std::move(pair));
+        return {MutableHandle{&_name_map, i}, inserted};
     }
 
     MutableHandle operator[](std::string key) {
+        //auto [i,inserted] = _name_map.insert({std::move(key), {}});
+        //return MutableHandle{&_name_map, i};
         return insert({std::move(key), ObjectId{}}).first;
     }
 
     std::set<ObjectId> children() const {
         std::set<ObjectId> ch;
-        for (auto& [_, id] : _impl) { ch.insert(id); }
+        for (auto& [_, id] : _name_map) { ch.insert(id); }
         return ch;
     }
 
@@ -127,7 +130,7 @@ public:
 
     template<class Archive>
     void serialize(Archive& ar, const unsigned int version) {
-        ar & _impl;
+        ar & _name_map;
     }
 
     ObjectId calculate_id() const;
@@ -135,7 +138,7 @@ public:
     friend std::ostream& operator<<(std::ostream&, const Tree&);
 
 private:
-    Impl _impl;
+    NameMap _name_map;
 };
 
 
