3ed55d17935674dcef31de98b9a674a00ab311c8
Adam Cigánek <adam.ciganek@gmail.com>
2021-04-07 14:16:37 +0200

    RIIR

--- src/ouisync_assert.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// With clang, the standard assert won't make the address sanitizer spit
-// out the stack trace.
-#define ouisync_assert(x) if (!(x)) { volatile void* p = nullptr; *((int*) p) = 1; }
--- src/versioned_object.cpp
+++
@@ -1,7 +0,0 @@
-#include "versioned_object.h"
-
-using namespace ouisync;
-
-std::ostream& ouisync::operator<<(std::ostream& os, const VersionedObject& c) {
-    return os << "VersionedObject{" << c.id << ", " << c.versions << "}";
-}
