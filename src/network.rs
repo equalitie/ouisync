d069b2355affe223ab10d2ee64311c0b9ccf0ce4
Peter Jankuliak <p.jankuliak@gmail.com>
2021-04-20 16:58:51 +0200

    Remove warnings from network.rs

--- src/network.rs
+++ src/network.rs
@@ -1,25 +1,35 @@
 use crate::replica_discovery::ReplicaDiscovery;
 use crate::replica_id::ReplicaId;
+use crate::object_stream::ObjectStream;
 
-use std::{io, sync::Arc, net::SocketAddr, collections::HashSet};
-use futures::future::{abortable, AbortHandle};
+use std::{
+    fmt,
+    io,
+    sync::Arc,
+    net::SocketAddr,
+    collections::{HashSet, HashMap},
+};
+
+use futures::future::{/*abortable, */AbortHandle};
 
 use tokio::{
     task::spawn,
     net::{TcpStream, TcpListener},
-    io::AsyncWriteExt,
+    sync::Mutex,
 };
 
 pub struct Network {
-    abort_handles: HashSet<AbortHandle>,
+    _abort_handles: HashSet<AbortHandle>,
     this_replica_id: ReplicaId,
+    replicas: Mutex<HashMap<ReplicaId, Replica>>,
 }
 
 impl Network {
     pub fn new(_enable_discovery: bool) -> io::Result<Arc<Network>> {
         let n = Arc::new(Network{
-            abort_handles: HashSet::new(),
+            _abort_handles: HashSet::new(),
             this_replica_id: ReplicaId::random(),
+            replicas: Mutex::new(HashMap::new()),
         });
         let n_ = n.clone();
         spawn(async move { n_.start().await.unwrap(); });
@@ -29,7 +39,7 @@ impl Network {
     async fn start(self: Arc<Self>) -> io::Result<()> {
         let any_addr = SocketAddr::from(([0,0,0,0], 0));
         let listener = TcpListener::bind(any_addr).await?;
-        
+
         let s1 = self.clone();
         let s2 = self.clone();
 
@@ -52,7 +62,7 @@ impl Network {
                         Ok(socket) => socket,
                         Err(_) => return,
                     };
-                    s.handle_new_connection(socket).await;
+                    s.handle_new_connection(ConnectionType::Connected, socket).await.ok();
                 });
             }
         }
@@ -60,16 +70,65 @@ impl Network {
 
     async fn run_listener(self: Arc<Self>, listener: TcpListener) -> io::Result<()> {
         loop {
-            let (socket, addr) = listener.accept().await?;
+            let (socket, _addr) = listener.accept().await?;
             let s = self.clone();
             spawn(async move {
-                s.handle_new_connection(socket).await;
+                s.handle_new_connection(ConnectionType::Accepted, socket).await.ok();
             });
         }
     }
 
-    async fn handle_new_connection(self: Arc<Self>, mut socket: TcpStream) -> io::Result<()> {
-        println!("Got new connection");
+    async fn handle_new_connection(self: Arc<Self>, con_type: ConnectionType, socket: TcpStream)
+        -> io::Result<()>
+    {
+        let mut os = ObjectStream::new(socket);
+        os.write(&self.this_replica_id).await?;
+        let their_replica_id = os.read::<ReplicaId>().await?;
+
+        let mut replicas = self.replicas.lock().await;
+
+        let replica = replicas
+            .entry(their_replica_id)
+            .or_insert(Replica::new());
+
+        match con_type {
+            ConnectionType::Accepted => {
+                replica.accepted_stream = Some(os);
+            },
+            ConnectionType::Connected => {
+                replica.connected_stream = Some(os);
+            }
+        }
+
+        println!("{:?}", replica);
         Ok(())
     }
+}
+
+struct Replica {
+    accepted_stream: Option<ObjectStream>,
+    connected_stream: Option<ObjectStream>,
+}
+
+impl Replica {
+    fn new() -> Replica {
+        Replica {
+            accepted_stream: None,
+            connected_stream: None,
+        }
+    }
+}
+
+impl fmt::Debug for Replica {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut c = 0;
+        if self.accepted_stream .is_some() { c += 1; }
+        if self.connected_stream.is_some() { c += 1; }
+        write!(f, "Replica:{:?}", c)
+    }
+}
+
+#[derive(Debug)]
+enum ConnectionType {
+    Accepted, Connected
 }
