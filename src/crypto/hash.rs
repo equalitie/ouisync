03433b35cde49729704ff64ade79fb29f51af26a
Peter Jankuliak <p.jankuliak@gmail.com>
2021-04-29 14:00:52 +0200

    Impl TryFrom<[u8] for Hash

--- src/crypto/hash.rs
+++ src/crypto/hash.rs
@@ -1,9 +1,17 @@
 use crate::format;
+use sha3::digest::generic_array::typenum::Unsigned;
 use sha3::{
-    digest::{generic_array::GenericArray, Digest},
+    digest::{
+        generic_array::{sequence::GenericSequence, GenericArray},
+        Digest,
+    },
     Sha3_256,
 };
-use std::fmt;
+use std::{
+    array::TryFromSliceError,
+    convert::{TryFrom, TryInto},
+    fmt,
+};
 
 /// Wrapper for a 256-bit hash digest, for convenience. Also implements friendly formatting.
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
@@ -37,6 +45,16 @@ impl fmt::Debug for Hash {
 impl fmt::LowerHex for Hash {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         format::hex(f, &self.0)
+    }
+}
+
+impl TryFrom<&'_ [u8]> for Hash {
+    type Error = TryFromSliceError;
+
+    fn try_from(slice: &[u8]) -> Result<Self, Self::Error> {
+        const USIZE: usize = <Inner as GenericSequence<_>>::Length::USIZE;
+        let slice: [u8; USIZE] = slice.try_into()?;
+        Ok(Self(slice.into()))
     }
 }
 
