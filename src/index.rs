70f5e4837c419f54f55b8391bf736bae2e183d88
Peter Jankuliak <p.jankuliak@gmail.com>
2021-05-04 15:36:54 +0200

    Remove index_leaves from DB schema
    Wasn't being used

--- src/index.rs
+++ src/index.rs
@@ -6,20 +6,20 @@ use crate::{
 /// Initializes the index. Creates the required database schema unless already exists.
 pub async fn init(pool: &db::Pool) -> Result<(), Error> {
     sqlx::query(
-        "CREATE TABLE IF NOT EXISTS index_leaves (
-             block_name    BLOB NOT NULL,
-             block_version BLOB NOT NULL,
-             locator       BLOB NOT NULL UNIQUE
-         );
-         CREATE TABLE IF NOT EXISTS branches (
+        "CREATE TABLE IF NOT EXISTS branches (
              id                 INTEGER PRIMARY KEY,
              root_block_name    BLOB NOT NULL,
              root_block_version BLOB NOT NULL,
              merkle_root        BLOB NOT NULL
          );
          CREATE TABLE IF NOT EXISTS merkle_forest (
+             /* Parent is a hash calculated from its children */
              parent  BLOB NOT NULL,
              bucket  INTEGER,
+             /*
+              * Child is a hash calculated from its children (as the `parent` is), or - if this is
+              * a leaf layer - child is a blob serialized from the locator hash and BlockId
+              */
              child   BLOB NOT NULL
          );",
     )
