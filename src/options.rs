db3156ca96a64b97ef480778a2cee88f61fd0c21
Adam Cigánek <adam.ciganek@gmail.com>
2021-06-01 13:01:36 +0200

    Allow specifying database name
    To allow running multiple instances on the same machine

--- src/options.rs
+++ src/options.rs
@@ -1,18 +1,18 @@
 use anyhow::{Context, Result};
 use ouisync::NetworkOptions;
-use std::{net::SocketAddr, path::PathBuf};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// Command line options.
 #[derive(StructOpt)]
 pub(crate) struct Options {
+    /// Database name
+    #[structopt(short = "n", long, default_value = "db")]
+    pub db_name: String,
+
     /// Mount directory
     #[structopt(short, long)]
     pub mount_dir: PathBuf,
-
-    /// Peer's endpoint
-    #[structopt(short, long, value_name = "ip:port")]
-    pub connect: Option<SocketAddr>,
 
     #[structopt(flatten)]
     pub network: NetworkOptions,
@@ -24,6 +24,6 @@ impl Options {
         Ok(dirs::data_dir()
             .context("failed to initialize data directory")?
             .join(env!("CARGO_PKG_NAME"))
-            .join("db"))
+            .join(&self.db_name))
     }
 }
