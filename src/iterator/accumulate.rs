4664dd5deb2e31254e8d469bd5f536c9d50b6293
Peter Jankuliak <p.jankuliak@gmail.com>
2021-06-29 15:09:11 +0200

    Modify iterator::Accumulate to not require Copy
    In input iterator.

--- src/iterator/accumulate.rs
+++ src/iterator/accumulate.rs
@@ -1,41 +1,53 @@
-use std::iter::Take;
+use std::iter::Peekable;
 
-pub struct Accumulate<Iter, GetKey> {
-    iter: Iter,
+pub struct Accumulate<Iter, Key, GetKey>
+where
+    Iter: Iterator,
+    GetKey: FnMut(Iter::Item) -> Key,
+    Key: Copy,
+{
+    iter: Peekable<Iter>,
     get_key: GetKey,
 }
 
-impl<Iter, GetKey> Accumulate<Iter, GetKey> {
+impl<Iter, Key, GetKey> Accumulate<Iter, Key, GetKey>
+where
+    Iter: Iterator,
+    GetKey: FnMut(Iter::Item) -> Key,
+    Key: Copy,
+{
     pub fn new(iter: Iter, get_key: GetKey) -> Self {
-        Self { iter, get_key }
+        Self {
+            iter: iter.peekable(),
+            get_key,
+        }
     }
 }
 
-impl<Iter: Iterator + Clone, Key, GetKey> Iterator for Accumulate<Iter, GetKey>
+impl<Iter: Iterator, Key, GetKey> Iterator for Accumulate<Iter, Key, GetKey>
 where
     GetKey: FnMut(Iter::Item) -> Key,
     Key: PartialEq + Copy,
+    Iter::Item: Copy,
 {
-    type Item = (Key, Take<Iter>);
+    type Item = (Key, Vec<Iter::Item>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut i = self.iter.clone();
-        let mut n: usize = 0;
+        // Note: This could be implemented without allocating the Vec buffer, but then we would
+        // need self.iter to be Copy-able, and unfortunately, what we get from
+        // iterator::union::new_from_many is not Copy-able.
+        let first = self.iter.next();
 
-        let first_key = i.next().map(|v| (self.get_key)(v));
+        if let Some(first) = first {
+            let get_key = &mut self.get_key;
+            let key = (get_key)(first);
+            let mut buf = vec![first];
 
-        if let Some(first_key) = first_key {
-            let mut j = i.clone();
-            loop {
-                n += 1;
-                if i.next().map(|v| (self.get_key)(v)) != Some(first_key) {
-                    break;
-                }
-                j = i.clone();
+            while let Some(v) = self.iter.next_if(|&e| (get_key)(e) == key) {
+                buf.push(v);
             }
-            let r = self.iter.clone().take(n);
-            self.iter = j;
-            Some((first_key, r))
+
+            Some((key, buf))
         } else {
             None
         }
@@ -51,8 +63,7 @@ mod tests {
         let v = &[1, 2, 2, 3, 4, 4, 4];
         let a = Accumulate::new(v.iter(), |i| i);
         assert_eq!(
-            a.map(|(k, t)| (k, t.collect::<Vec<_>>()))
-                .collect::<Vec<_>>(),
+            a.map(|(k, t)| (k, t)).collect::<Vec<_>>(),
             [
                 (&1, vec![&1]),
                 (&2, vec![&2, &2]),
