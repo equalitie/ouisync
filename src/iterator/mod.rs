306a6e9f09cc0e233dec386e6529794a8411a3cd
Adam Cigánek <adam.ciganek@gmail.com>
2021-08-03 15:01:13 +0200

    Remove unneeded DrainFilter

--- src/iterator/mod.rs
+++ src/iterator/mod.rs
@@ -1,5 +1,4 @@
 mod accumulate;
-mod drain_filter;
 mod sorted_union;
 
-pub use self::{accumulate::Accumulate, drain_filter::DrainFilter, sorted_union::SortedUnion};
+pub use self::{accumulate::Accumulate, sorted_union::SortedUnion};
