c772315ee862937750461a4a7da85bf6d70c5122
Adam Cigánek <adam.ciganek@gmail.com>
2021-09-23 11:53:40 +0200

    Implement sqlx Type, Decode traits for RepositoryId

--- src/repository/index_map.rs
+++ src/repository/index_map.rs
@@ -1,6 +1,12 @@
 use super::manager::RepositoryId;
-use crate::{error::Result, index::Index, replica_id::ReplicaId};
+use crate::{
+    db,
+    error::{Error, Result},
+    index::Index,
+    replica_id::ReplicaId,
+};
 use futures_util::{future, TryStreamExt};
+use sqlx::Row;
 use std::collections::HashMap;
 
 /// Map of repository indices.
@@ -11,31 +17,34 @@ pub(crate) struct IndexMap {
 }
 
 impl IndexMap {
-    // pub async fn new(main_pool: db::Pool, this_replica_id: ReplicaId) -> Result<Self> {
-    //     let mut indices = HashMap::new();
-    //     let mut ids = HashMap::new();
+    pub async fn new(main_pool: db::Pool, this_replica_id: ReplicaId) -> Result<Self> {
+        let mut indices = HashMap::new();
+        let mut ids = HashMap::new();
 
-    //     sqlx::query("SELECT rowid, name, db_path FROM repositories")
-    //         .fetch(&main_pool)
-    //         .err_into()
-    //         .and_then(|row| async move {
-    //             todo!()
-    //             // let store = row.get(2).parse();
-    //             // let repo_pool =
-    //             // let index = Index::
-    //         })
-    //         .try_for_each(|(id, name, index)| {
-    //             indices.insert(id, index);
-    //             ids.insert(name, id);
+        sqlx::query("SELECT rowid, name, db_path FROM repositories")
+            .fetch(&main_pool)
+            .err_into()
+            .and_then(|row| async move {
+                // `unwrap` is OK because the error type here is `Infallible`.
+                // TODO: replace with `into_ok` when stabilized.
+                let store = row.get::<&str, _>(2).parse().unwrap();
+                let pool = db::init(store).await?;
+                let index = Index::load(pool, this_replica_id).await?;
 
-    //             future::ready(Ok(()))
-    //         })
-    //         .await?;
+                Ok::<_, Error>((row.get(0), row.get(1), index))
+            })
+            .try_for_each(|(id, name, index)| {
+                indices.insert(id, index);
+                ids.insert(name, id);
 
-    //     Self {
-    //         this_replica_id,
-    //         indices,
-    //         ids,
-    //     }
-    // }
+                future::ready(Ok(()))
+            })
+            .await?;
+
+        Ok(Self {
+            this_replica_id,
+            indices,
+            ids,
+        })
+    }
 }
