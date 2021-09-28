db7a283d9264187acc4be6a3075b533e50f938b8
Adam Cigánek <adam.ciganek@gmail.com>
2021-09-28 14:04:03 +0200

    Implement RepositoryManager::delete

--- src/repository/manager.rs
+++ src/repository/manager.rs
@@ -81,8 +81,32 @@ impl RepositoryManager {
     }
 
     /// Deletes an existing repository.
-    pub async fn delete(&mut self, _name: &str) -> Result<()> {
-        todo!()
+    pub async fn delete(&mut self, name: &str) -> Result<()> {
+        let mut tx = self.pool.begin().await?;
+
+        let row = sqlx::query("SELECT id, path FROM repositories WHERE name = ?")
+            .bind(name)
+            .fetch_one(&mut tx)
+            .await?;
+
+        let id: u32 = row.get(0);
+        let store = row.get(1);
+
+        sqlx::query("DELETE FROM repositories WHERE id = ? LIMIT 1")
+            .bind(id)
+            .execute(&mut tx)
+            .await?;
+
+        let repository = self.repositories.remove(name).ok_or(Error::EntryNotFound)?;
+
+        // TODO: send delete notification
+
+        repository.index().pool.close().await;
+        db::delete(store).await?;
+
+        tx.commit().await?;
+
+        Ok(())
     }
 
     /// Gets a repository with the specified name or `None` if no such repository exists.
@@ -135,86 +159,3 @@ pub(crate) async fn init(pool: &db::Pool) -> Result<()> {
 
     Ok(())
 }
-
-/*
-/// Map of repository indices.
-pub(crate) struct IndexMap {
-    values: HashMap<RepositoryId, Value>,
-    ids: HashMap<RepositoryName, RepositoryId>,
-}
-
-impl IndexMap {
-    pub fn new() -> Self {
-        Self {
-            values: HashMap::new(),
-            ids: HashMap::new(),
-        }
-    }
-
-    /// Insert an index into this map. Returns `EntryExist` error if either `id` or `name` already
-    /// exists in the map.
-    pub fn insert(&mut self, id: RepositoryId, name: RepositoryName, index: Index) -> Result<()> {
-        match (self.values.entry(id), self.ids.entry(name.clone())) {
-            (hash_map::Entry::Vacant(value_entry), hash_map::Entry::Vacant(id_entry)) => {
-                value_entry.insert(Value { index, name });
-                id_entry.insert(id);
-                Ok(())
-            }
-            _ => {
-                // TODO: should we have a separate error variant (e.g. `RepositoryExists`) for this?
-                Err(Error::EntryExists)
-            }
-        }
-    }
-
-
-    // pub async fn destroy(&mut self, id: RepositoryId) -> Result<()> {
-    //     let mut tx = self.pool.begin().await?;
-
-    //     let store = sqlx::query("SELECT db_path FROM repositories WHERE id = ?")
-    //         .bind(id)
-    //         .fetch_one(&mut tx)
-    //         .await?
-    //         .get(0);
-
-    //     sqlx::query("DELETE FROM repositories WHERE id = ? LIMIT 1")
-    //         .bind(id)
-    //         .execute(&mut tx)
-    //         .await?;
-
-    //     let value = self.values.remove(&id).ok_or(Error::EntryNotFound)?;
-    //     self.ids.remove(&value.name);
-
-    //     value.index.pool.close().await;
-
-    //     db::delete(store).await?;
-
-    //     tx.commit().await?;
-
-    //     Ok(())
-    // }
-
-    pub fn get(&self, id: RepositoryId) -> Option<&Index> {
-        self.values.get(&id).map(|value| &value.index)
-    }
-
-    pub fn lookup(&self, name: &str) -> Option<(RepositoryId, &RepositoryName, &Index)> {
-        let id = self.ids.get(name)?;
-        let value = &self.values.get(id)?;
-
-        Some((*id, &value.name, &value.index))
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item = (RepositoryId, &RepositoryName, &Index)> {
-        self.ids.iter().filter_map(move |(name, id)| {
-            self.values.get(id).map(|value| (*id, name, &value.index))
-        })
-    }
-}
-
-struct Value {
-    index: Index,
-    name: RepositoryName,
-}
-
-*/
