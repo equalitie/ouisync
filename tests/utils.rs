532004fea8bcff4f1c3361f9d804a6f51a7417e2
Peter Jankuliak <p.jankuliak@gmail.com>
2021-10-20 15:41:29 +0200

    Disable UPnP for integration tests

--- tests/utils.rs
+++ tests/utils.rs
@@ -25,6 +25,7 @@ impl Bin {
             .arg("--mount")
             .arg(format!("test:{}", mount_dir.path().display()))
             .arg("--print-ready-message")
+            .arg("--disable-upnp")
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
             .spawn()
