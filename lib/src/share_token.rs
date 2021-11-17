a84f49351ee538b1d087bed8f3fb55a2c8c16e27
Adam Cigánek <adam.ciganek@gmail.com>
2021-11-17 09:41:10 +0100

    Implement FromStr and Display for ShareToken

--- lib/src/share_token.rs
+++ lib/src/share_token.rs
@@ -1,30 +1,42 @@
 use crate::repository::RepositoryId;
+use std::{fmt, str::FromStr};
 use thiserror::Error;
 use url::Url;
 
 pub const SCHEME: &str = "ouisync";
 
-/// Token to share a repository. Can be transmitted to other replicas by various means.
+/// Token to share a repository which can be encoded as a URL-formatted string and transmitted to
+/// other replicas.
 #[derive(Eq, PartialEq, Debug)]
 pub struct ShareToken {
     pub(crate) id: RepositoryId,
 }
 
 impl ShareToken {
-    // Encode this share token as an URL.
-    pub fn to_url(&self) -> Url {
-        Url::parse(&format!("{}:{:x}", SCHEME, self.id)).unwrap()
+    pub fn new(id: RepositoryId) -> Self {
+        Self { id }
     }
+}
 
-    // Decode a share token from an URL.
-    pub fn from_url(url: &Url) -> Result<Self, DecodeError> {
+impl FromStr for ShareToken {
+    type Err = DecodeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let url = Url::parse(s)?;
+
         if url.scheme() != SCHEME {
             return Err(DecodeError);
         }
 
-        let id = url.path().parse().map_err(|_| DecodeError)?;
+        let id = url.path().parse()?;
 
         Ok(Self { id })
+    }
+}
+
+impl fmt::Display for ShareToken {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{:x}", SCHEME, self.id)
     }
 }
 
@@ -32,29 +44,40 @@ impl ShareToken {
 #[error("failed to decode share token")]
 pub struct DecodeError;
 
+impl From<hex::FromHexError> for DecodeError {
+    fn from(_: hex::FromHexError) -> Self {
+        Self
+    }
+}
+
+impl From<url::ParseError> for DecodeError {
+    fn from(_: url::ParseError) -> Self {
+        Self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn to_url() {
+    fn encode() {
         let id_hex = "416d9c3fe32017f7b5c8e406630576ad";
         let id_bytes = hex::decode(id_hex).unwrap();
         let id = RepositoryId::try_from(id_bytes.as_ref()).unwrap();
 
         let token = ShareToken { id };
-        let url = token.to_url();
 
-        assert_eq!(url.to_string(), format!("ouisync:{}", id_hex))
+        assert_eq!(token.to_string(), format!("ouisync:{}", id_hex))
     }
 
     #[test]
-    fn to_and_from_url() {
+    fn encode_and_decode() {
         let id: RepositoryId = rand::random();
         let token = ShareToken { id };
 
-        let url = token.to_url();
-        let decoded = ShareToken::from_url(&url).unwrap();
+        let string = token.to_string();
+        let decoded: ShareToken = string.parse().unwrap();
         assert_eq!(decoded, token);
     }
 }
