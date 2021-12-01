8432ddebdfb9e497e7bbf82a93f3fdeb87985b48
Adam Cigánek <adam.ciganek@gmail.com>
2021-12-01 11:59:32 +0100

    Add function to derive sub-key from master SecretKey and nonce

--- lib/src/crypto/mod.rs
+++ lib/src/crypto/mod.rs
@@ -12,6 +12,7 @@ pub use chacha20poly1305::aead;
 
 use self::aead::{AeadInPlace, NewAead};
 use chacha20poly1305::ChaCha20Poly1305;
+use generic_array::{sequence::GenericSequence, typenum::Unsigned};
 
 /// Authentication tag.
 pub type AuthTag = chacha20poly1305::Tag;
@@ -57,4 +58,36 @@ impl Cryptor {
             Self::Null => Ok(()),
         }
     }
+}
+
+pub(crate) fn encrypt(
+    key: &SecretKey,
+    nonce: u64,
+    aad: &[u8],
+    buffer: &mut [u8],
+) -> Result<AuthTag, aead::Error> {
+    ChaCha20Poly1305::new(key.as_array()).encrypt_in_place_detached(&make_nonce(nonce), aad, buffer)
+}
+
+pub(crate) fn decrypt(
+    key: &SecretKey,
+    nonce: u64,
+    aad: &[u8],
+    buffer: &mut [u8],
+    auth_tag: &AuthTag,
+) -> Result<(), aead::Error> {
+    ChaCha20Poly1305::new(key.as_array()).decrypt_in_place_detached(
+        &make_nonce(nonce),
+        aad,
+        buffer,
+        auth_tag,
+    )
+}
+
+fn make_nonce(counter: u64) -> Nonce {
+    const LEN: usize = <<Nonce as GenericSequence<_>>::Length as Unsigned>::USIZE;
+
+    let mut nonce = Nonce::default();
+    nonce[LEN - 8..].copy_from_slice(&counter.to_le_bytes());
+    nonce
 }
