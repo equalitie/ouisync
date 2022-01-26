dd24822ea2fc9377dcc0b47c8c0e972c5afe9a30
Adam Cigánek <adam.ciganek@gmail.com>
2022-01-26 11:27:32 +0100

    ffi: Add ErrorCode enum

--- lib/src/ffi/error.rs
+++ lib/src/ffi/error.rs
@@ -5,45 +5,61 @@ use std::io;
 pub enum ErrorCode {
     /// No error
     Ok = 0,
+    /// Database error
     Db = 1,
+    /// Insuficient permission to perform the intended operation
     PermissionDenied = 2,
+    /// Malformed data
     MalformedData = 3,
-    BlockNotFound = 4,
-    // BlockExists,
-    // #[error("block is not referenced by the index")]
-    // BlockNotReferenced,
-    // #[error("block has wrong length (expected: {}, actual: {0})", BLOCK_SIZE)]
-    // WrongBlockLength(usize),
-    // #[error("not a directory or directory malformed")]
-    // MalformedDirectory(#[source] bincode::Error),
-    // #[error("entry already exists")]
-    // EntryExists,
-    // #[error("entry not found")]
-    // EntryNotFound,
-    // #[error("entry has multiple concurrent versions")]
-    // AmbiguousEntry,
-    // #[error("entry is a file")]
-    // EntryIsFile,
-    // #[error("entry is a directory")]
-    // EntryIsDirectory,
-    // #[error("File name is not a valid UTF-8 string")]
-    // NonUtf8FileName,
-    // #[error("offset is out of range")]
-    // OffsetOutOfRange,
-    // #[error("directory is not empty")]
-    // DirectoryNotEmpty,
-    // #[error("operation is not supported")]
-    // OperationNotSupported,
-    // #[error("network error")]
-    // Network(#[source] io::Error),
-    // #[error("writer set error")]
-    // WriterSet(crate::writer_set::error::Error),
+    /// Entry already exists
+    EntryExists = 4,
+    /// Entry doesn't exist
+    EntryNotFound = 5,
+    /// Multiple matching entries found
+    AmbiguousEntry = 6,
+    /// The intended operation requires the directory to be empty but it isn't
+    DirectoryNotEmpty = 7,
+    /// The indended operation is not supported
+    OperationNotSupported = 8,
+    /// Network error
+    Network = 9,
+    /// Unspecified error
+    Other = u32::MAX,
 }
 
 pub(crate) trait ToErrorCode {
     fn to_error_code(&self) -> ErrorCode;
 }
 
-impl ToErrorCode for Error {}
+impl ToErrorCode for Error {
+    fn to_error_code(&self) -> ErrorCode {
+        match self {
+            Self::CreateDbDirectory(_)
+            | Self::ConnectToDb(_)
+            | Self::CreateDbSchema(_)
+            | Self::QueryDb(_) => ErrorCode::Db,
+            Self::PermissionDenied => ErrorCode::PermissionDenied,
+            Self::MalformedData | Self::MalformedDirectory(_) => ErrorCode::MalformedData,
+            Self::EntryExists => ErrorCode::EntryExists,
+            Self::EntryNotFound => ErrorCode::EntryNotFound,
+            Self::AmbiguousEntry => ErrorCode::AmbiguousEntry,
+            Self::DirectoryNotEmpty => ErrorCode::DirectoryNotEmpty,
+            Self::OperationNotSupported => ErrorCode::OperationNotSupported,
+            Self::Network(_) => ErrorCode::Network,
+            Self::BlockNotFound(_)
+            | Self::BlockNotReferenced
+            | Self::WrongBlockLength(_)
+            | Self::EntryIsFile
+            | Self::EntryIsDirectory
+            | Self::NonUtf8FileName
+            | Self::OffsetOutOfRange
+            | Self::WriterSet(_) => ErrorCode::Other,
+        }
+    }
+}
 
-impl ToErrorCode for io::Error {}
+impl ToErrorCode for io::Error {
+    fn to_error_code(&self) -> ErrorCode {
+        ErrorCode::Other
+    }
+}
