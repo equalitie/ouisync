e58716f67c84278619aac7d1e5406e400ac14a5e
Peter Jankuliak <p.jankuliak@gmail.com>
2022-02-16 10:29:19 +0100

    Add FFI API to show local network endpoint

--- lib/src/ffi/network.rs
+++ lib/src/ffi/network.rs
@@ -1,8 +1,9 @@
 use super::{
     session,
-    utils::{Port, UniqueHandle},
+    utils::{self, Port, UniqueHandle},
 };
 use crate::network::NetworkEvent;
+use std::{os::raw::c_char, ptr};
 use tokio::task::JoinHandle;
 
 pub const NETWORK_EVENT_PROTOCOL_VERSION_MISMATCH: u8 = 0;
@@ -26,5 +27,23 @@ pub unsafe extern "C" fn network_subscribe(port: Port<u8>) -> UniqueHandle<JoinH
 fn encode_network_event(event: NetworkEvent) -> u8 {
     match event {
         NetworkEvent::ProtocolVersionMismatch => NETWORK_EVENT_PROTOCOL_VERSION_MISMATCH,
+    }
+}
+
+/// Return the local network endpoint as string. The format is
+/// "<TCP or UDP>:<IPv4 or [IPv6]>:<PORT>". Examples:
+///
+/// For IPv4: "TCP:192.168.1.1:65522"
+/// For IPv6: "TCP:[2001:db8::1]:65522"
+#[no_mangle]
+pub unsafe extern "C" fn network_local_addr() -> *const c_char {
+    let local_addr = session::get().network().local_addr();
+
+    // TODO: Get <TCP or UDP> from the network object.
+
+    if let Ok(s) = utils::str_to_c_string(&format!("TCP:{}", local_addr)) {
+        s.into_raw()
+    } else {
+        ptr::null()
     }
 }
