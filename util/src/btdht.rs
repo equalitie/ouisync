3922cb5e8177c8f20048f5ff17be5acba1801f29
Peter Jankuliak <p.jankuliak@gmail.com>
2022-02-18 15:57:05 +0100

    Also use IPv6 in the new btdht utility

--- util/src/btdht.rs
+++ util/src/btdht.rs
@@ -4,13 +4,13 @@ use ouisync_lib::{
     network::{self, dht_discovery},
     ShareToken,
 };
-use std::{collections::HashSet, io, net::Ipv4Addr};
+use std::{collections::HashSet, io, net::{Ipv4Addr, Ipv6Addr}};
 use structopt::StructOpt;
 use tokio::{net::UdpSocket, task};
 
 /// Command line options.
 #[derive(StructOpt, Debug)]
-pub(crate) struct Options {
+struct Options {
     /// Accept a share token.
     #[structopt(long, value_name = "TOKEN")]
     pub token: ShareToken,
@@ -22,42 +22,63 @@ async fn main() -> io::Result<()> {
 
     env_logger::init();
 
-    // TODO: IPv6
-
-    let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).await?;
+    let socket_v4 = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).await.ok();
+    let socket_v6 = UdpSocket::bind((Ipv6Addr::UNSPECIFIED, 0)).await.ok();
 
     println!("Resolving DHT routers...");
-    let (ipv4_routers, _ipv6_routers) = dht_discovery::dht_router_addresses().await;
+    let (ipv4_routers, ipv6_routers) = dht_discovery::dht_router_addresses().await;
 
-    let dht = MainlineDht::builder()
-        .add_routers(ipv4_routers)
-        .set_read_only(true)
-        .start(socket);
+    for router in ipv4_routers.iter().chain(ipv6_routers.iter()) {
+        println!("  {:?}", router);
+    }
 
-    let task = task::spawn( async move {
-        println!("Bootstrapping...");
+    let dht_v4 = socket_v4.map(|socket|
+        MainlineDht::builder()
+            .add_routers(ipv4_routers)
+            .set_read_only(true)
+            .start(socket)
+    );
 
-        if dht.bootstrapped().await {
-            println!("Bootstrap complete");
-            let info_hash = network::repository_info_hash(options.token.id());
+    let dht_v6 = socket_v6.map(|socket|
+        MainlineDht::builder()
+            .add_routers(ipv6_routers)
+            .set_read_only(true)
+            .start(socket)
+    );
 
-            println!("Searching for peers...");
-            let mut seen_peers = HashSet::new();
-            let mut peers = dht.search(info_hash, false);
+    let task = task::spawn(async move {
+        if let Some(dht) = dht_v4 {
+            println!("");
+            lookup("IPv4", &dht, &options.token).await;
+        }
 
-            while let Some(peer) = peers.next().await {
-                if seen_peers.insert(peer) {
-                    println!("    {:?}", peer);
-                }
-            }
-
-            println!("Done");
-        } else {
-            println!("Bootstrap failed")
+        if let Some(dht) = dht_v6 {
+            println!("");
+            lookup("IPv6", &dht, &options.token).await;
         }
     });
 
     task.await?;
 
     Ok(())
+}
+
+async fn lookup(prefix: &str, dht: &MainlineDht, token: &ShareToken) {
+    println!("{} Bootstrapping...", prefix);
+    
+    if dht.bootstrapped().await {
+        let mut seen_peers = HashSet::new();
+        let info_hash = network::repository_info_hash(token.id());
+    
+        println!("{} Searching for peers...", prefix);
+        let mut peers = dht.search(info_hash, false);
+    
+        while let Some(peer) = peers.next().await {
+            if seen_peers.insert(peer) {
+                println!("  {:?}", peer);
+            }
+        }
+    } else {
+        println!("{} Bootstrap failed", prefix)
+    }
 }
