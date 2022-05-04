bf958e43007c20795038982ba88d152e615dd027
Adam Cigánek <adam.ciganek@gmail.com>
2022-05-04 16:04:14 +0200

    Add (currently failing, ignored) tests for redownloading deleted file

--- lib/tests/gc.rs
+++ lib/tests/gc.rs
@@ -2,7 +2,7 @@
 
 use ouisync::{File, Repository, BLOB_HEADER_SIZE, BLOCK_SIZE};
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use std::time::Duration;
+use std::{io::SeekFrom, time::Duration};
 use tokio::time;
 
 mod common;
@@ -164,6 +164,54 @@ async fn remote_truncate_remote_file() {
 
     // 1 block for the file + 1 block for the remote root
     time::timeout(Duration::from_secs(5), expect_block_count(&repo_l, 2))
+        .await
+        .unwrap();
+}
+
+// FIXME: this currently fails because the first block of the file is not changed by the update and
+// so is never redownloaded.
+#[ignore]
+#[tokio::test(flavor = "multi_thread")]
+async fn concurrent_delete_update() {
+    let mut rng = StdRng::seed_from_u64(0);
+
+    let (network_l, network_r) = common::create_connected_peers().await;
+    let (repo_l, repo_r) = common::create_linked_repos(&mut rng).await;
+    let reg_l = network_l.handle().register(repo_l.index().clone());
+    let reg_r = network_r.handle().register(repo_r.index().clone());
+
+    let mut file = repo_r.create_file("test.dat").await.unwrap();
+    write_to_file(&mut rng, &mut file, BLOCK_SIZE - BLOB_HEADER_SIZE).await;
+    file.flush().await.unwrap();
+
+    // 1 for the remote root + 1 for the file
+    time::timeout(Duration::from_secs(5), expect_block_count(&repo_l, 2))
+        .await
+        .unwrap();
+
+    // Disconnect to allow concurrent modifications.
+    drop(reg_l);
+    drop(reg_r);
+
+    // Local delete
+    repo_l.remove_entry("test.dat").await.unwrap();
+    repo_l.collect_garbage().await.unwrap();
+
+    // Sanity check
+    assert_eq!(repo_l.count_blocks().await.unwrap(), 1);
+
+    // Remote update. Don't change the length of the file so the first block (where the length it
+    // stored) remains unchanged.
+    file.seek(SeekFrom::End(-64)).await.unwrap();
+    write_to_file(&mut rng, &mut file, 64).await;
+    file.flush().await.unwrap();
+
+    // Re-connect
+    let _reg_l = network_l.handle().register(repo_l.index().clone());
+    let _reg_r = network_r.handle().register(repo_r.index().clone());
+
+    // 1 for the local root + 1 for the remote root + 2 for the file
+    time::timeout(Duration::from_secs(5), expect_block_count(&repo_l, 4))
         .await
         .unwrap();
 }
