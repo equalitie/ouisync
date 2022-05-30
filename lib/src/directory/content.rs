7e3b6011d6cd1474aa93d0382ae2e7bda3fe733f
Adam Cigánek <adam.ciganek@gmail.com>
2022-05-30 15:18:15 +0200

    Implement automatic v0 -> v1 conversion of directory format

--- lib/src/directory/content.rs
+++ lib/src/directory/content.rs
@@ -12,8 +12,8 @@ pub(crate) const VERSION: u64 = 1;
 
 pub(super) type Content = ContentV1;
 
-type ContentV0 = BTreeMap<String, BTreeMap<PublicKey, EntryData>>;
 type ContentV1 = BTreeMap<String, EntryData>;
+type ContentV0 = BTreeMap<String, BTreeMap<PublicKey, EntryData>>;
 
 pub(super) fn serialize(content: &Content) -> Vec<u8> {
     let mut output = Vec::new();
@@ -25,9 +25,34 @@ pub(super) fn serialize(content: &Content) -> Vec<u8> {
 pub(super) fn deserialize(mut input: &[u8]) -> Result<Content> {
     let version = vint64::decode(&mut input).map_err(|_| Error::MalformedDirectory)?;
 
-    if version == VERSION {
-        bincode::deserialize(input).map_err(|_| Error::MalformedDirectory)
-    } else {
-        Err(Error::StorageVersionMismatch)
+    match version {
+        VERSION => bincode::deserialize(input).map_err(|_| Error::MalformedDirectory),
+        0 => Ok(upgrade_from_v0(
+            bincode::deserialize(input).map_err(|_| Error::MalformedDirectory)?,
+        )),
+        _ => Err(Error::StorageVersionMismatch),
     }
+}
+
+fn upgrade_from_v0(v0: ContentV0) -> ContentV1 {
+    use crate::versioned_file_name;
+
+    let mut v1 = BTreeMap::new();
+
+    for (name, versions) in v0 {
+        if versions.len() <= 1 {
+            // If there is only one version, insert it directly
+            if let Some(data) = versions.into_values().next() {
+                v1.insert(name, data);
+            }
+        } else {
+            // If there is more than one version, create unique name for each of them and insert
+            // them as separate entries
+            for (author_id, data) in versions {
+                v1.insert(versioned_file_name::create(&name, &author_id), data);
+            }
+        }
+    }
+
+    v1
 }
