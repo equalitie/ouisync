ccd3fd216e90a5df313e04949aa3b992ac4b4456
Adam Cigánek <adam.ciganek@gmail.com>
2022-07-25 11:49:23 +0200

    Use tracing::instrument attribute instead of debug::instrument in Worker

--- lib/src/debug.rs
+++ lib/src/debug.rs
@@ -1,6 +1,3 @@
-use crate::error::Result;
-use std::{fmt::Debug, future::Future, mem};
-
 #[derive(Clone)]
 pub struct DebugPrinter {
     // Used for indentation
@@ -54,26 +51,4 @@ impl Default for DebugPrinter {
     fn default() -> Self {
         DebugPrinter::new()
     }
-}
-
-pub(crate) async fn instrument<F, T>(label: &str, task: F) -> Result<T>
-where
-    F: Future<Output = Result<T>>,
-    T: Debug,
-{
-    struct LogOnDrop<'a>(&'a str);
-
-    impl<'a> Drop for LogOnDrop<'a> {
-        fn drop(&mut self) {
-            tracing::trace!("{}: [cancelled]", self.0);
-        }
-    }
-
-    let log_on_drop = LogOnDrop(label);
-    let result = task.await;
-
-    tracing::trace!("{}: {:?}", label, result);
-    mem::forget(log_on_drop);
-
-    result
 }
