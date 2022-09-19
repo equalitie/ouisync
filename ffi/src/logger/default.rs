89eb538e02c351d60297362e309aae7239926b41
Adam Cigánek <adam.ciganek@proton.me>
2022-09-19 11:10:40 +0200

    ffi: Disable log output by default on non-android

--- ffi/src/logger/default.rs
+++ ffi/src/logger/default.rs
@@ -1,5 +1,6 @@
 use ouisync_lib::StateMonitor;
 use std::{io, sync::Once};
+use tracing::metadata::LevelFilter;
 use tracing_subscriber::EnvFilter;
 
 pub(crate) struct Logger;
@@ -9,7 +10,13 @@ impl Logger {
         static LOG_INIT: Once = Once::new();
         LOG_INIT.call_once(|| {
             tracing_subscriber::fmt()
-                .with_env_filter(EnvFilter::from_default_env())
+                .with_env_filter(
+                    EnvFilter::builder()
+                        // Only show the logs if explicitly enabled with the `RUST_LOG` env
+                        // variable.
+                        .with_default_directive(LevelFilter::OFF.into())
+                        .from_env_lossy(),
+                )
                 .with_file(true)
                 .with_line_number(true)
                 .init()
