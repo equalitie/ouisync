e86f6554ba83b9495fb60e0981c798ab768c9986
Peter Jankuliak <p.jankuliak@gmail.com>
2022-10-20 16:17:25 +0200

    Show tracing records as MonitoredValues

--- lib/src/state_monitor/tracing_layer.rs
+++ lib/src/state_monitor/tracing_layer.rs
@@ -1,8 +1,12 @@
-use super::StateMonitor;
-use std::{collections::HashMap, fmt, sync::Mutex};
+use super::{MonitoredValue, StateMonitor};
+use std::{
+    collections::{hash_map, HashMap},
+    fmt,
+    sync::Mutex,
+};
 use tracing::{
     field::{Field, Visit},
-    span::{self, Attributes},
+    span::{self, Attributes, Record},
 };
 use tracing_subscriber::{layer::Context, registry::LookupSpan, Layer};
 
@@ -21,9 +25,11 @@ impl TracingLayer {
     }
 }
 
+type MonitoredValues = HashMap<&'static str, MonitoredValue<String>>;
+
 struct TraceLayerInner {
     root_monitor: StateMonitor,
-    spans: HashMap<SpanId, StateMonitor>,
+    spans: HashMap<SpanId, (StateMonitor, MonitoredValues)>,
 }
 
 type SpanId = u64;
@@ -36,11 +42,11 @@ impl<S: tracing::Subscriber + for<'lookup> LookupSpan<'lookup>> Layer<S> for Tra
         let span = ctx.span(id).unwrap();
 
         let parent_monitor = match span.parent() {
-            Some(parent_span) => inner.spans.get_mut(&parent_span.id().into_u64()).unwrap(),
+            Some(parent_span) => &mut inner.spans.get_mut(&parent_span.id().into_u64()).unwrap().0,
             None => &mut inner.root_monitor,
         };
 
-        let mut visitor = Visitor::new();
+        let mut visitor = AttrsVisitor::new();
         attrs.values().record(&mut visitor);
         let title = format!("{}({})", span.name(), visitor.to_str());
 
@@ -49,20 +55,35 @@ impl<S: tracing::Subscriber + for<'lookup> LookupSpan<'lookup>> Layer<S> for Tra
         // disambiguator that is not presented to the user.
         let span_monitor = parent_monitor.make_non_unique_child(title, id.into_u64());
 
-        assert!(inner.spans.insert(id.into_u64(), span_monitor).is_none());
+        let overwritten = inner
+            .spans
+            .insert(id.into_u64(), (span_monitor, MonitoredValues::new()))
+            .is_some();
+
+        assert!(!overwritten);
     }
 
     fn on_close(&self, id: span::Id, _ctx: Context<'_, S>) {
         let mut inner = self.inner.lock().unwrap();
         inner.spans.remove(&id.into_u64());
     }
+
+    fn on_record(&self, id: &span::Id, record: &Record<'_>, _ctx: Context<'_, S>) {
+        let mut inner = self.inner.lock().unwrap();
+
+        if let Some((monitor, values)) = inner.spans.get_mut(&id.into_u64()) {
+            record.record(&mut RecordVisitor { monitor, values });
+        }
+    }
 }
 
-struct Visitor {
+//--------------------------------------------------------------------
+
+struct AttrsVisitor {
     vec: Vec<String>,
 }
 
-impl Visitor {
+impl AttrsVisitor {
     fn new() -> Self {
         Self { vec: Vec::new() }
     }
@@ -72,8 +93,40 @@ impl Visitor {
     }
 }
 
-impl Visit for Visitor {
+impl Visit for AttrsVisitor {
     fn record_debug(&mut self, field: &Field, value: &dyn fmt::Debug) {
         self.vec.push(format!("{}={:?}", field.name(), value));
     }
 }
+
+//--------------------------------------------------------------------
+
+struct RecordVisitor<'a> {
+    monitor: &'a mut StateMonitor,
+    values: &'a mut MonitoredValues,
+}
+
+impl<'a> RecordVisitor<'a> {
+    fn set_value(&mut self, name: &'static str, value: String) {
+        match self.values.entry(name) {
+            hash_map::Entry::Occupied(mut entry) => {
+                *entry.get_mut().get() = value;
+            }
+            hash_map::Entry::Vacant(entry) => {
+                let value = self.monitor.make_value::<String>(name.into(), value);
+                entry.insert(value);
+            }
+        }
+    }
+}
+
+impl<'a> Visit for RecordVisitor<'a> {
+    fn record_debug(&mut self, field: &Field, value: &dyn fmt::Debug) {
+        self.set_value(field.name(), format!("{:?}", value));
+    }
+    fn record_str(&mut self, field: &Field, value: &str) {
+        self.set_value(field.name(), value.into());
+    }
+}
+
+//--------------------------------------------------------------------
