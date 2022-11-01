ea21c97f7f2342d0c77a465ad827bceb1042d2ba
Adam Cigánek <adam.ciganek@proton.me>
2022-11-01 10:36:37 +0100

    Update v3 migration to preserve summaries with zero missing blocks

--- lib/src/db/migrations/v3.sql
+++ lib/src/db/migrations/v3.sql
@@ -1,10 +1,24 @@
 ALTER TABLE snapshot_root_nodes RENAME COLUMN missing_blocks_count TO block_presence;
 ALTER TABLE snapshot_root_nodes DROP COLUMN missing_blocks_checksum;
-UPDATE snapshot_root_nodes SET block_presence = 0;
+
+-- If there were zero missing blocks, set the block_presence to 0xffffffffffffffff to indicate that
+-- no blocks are missing. Otherwise set it to 0 indicating that all blocks are missing to force the
+-- value to be updated on the next sync.
+UPDATE snapshot_root_nodes SET
+    block_presence = CASE block_presence
+        WHEN 0 THEN 0xffffffffffffffff
+        ELSE 0
+    END;
 
 ALTER TABLE snapshot_inner_nodes RENAME COLUMN missing_blocks_count TO block_presence;
 ALTER TABLE snapshot_inner_nodes DROP COLUMN missing_blocks_checksum;
-UPDATE snapshot_inner_nodes SET block_presence = 0;
+
+-- Same as for snapshot_root_nodes
+UPDATE snapshot_inner_nodes SET
+    block_presence = CASE block_presence
+        WHEN 0 THEN 0xffffffffffffffff
+        ELSE 0
+END;
 
 ALTER TABLE received_inner_nodes RENAME COLUMN missing_blocks_count TO block_presence;
 ALTER TABLE received_inner_nodes DROP COLUMN missing_blocks_checksum;
