90684c9f1295c7760bbdc6aecaa4a76554d0b719
Adam Cigánek <adam.ciganek@proton.me>
2022-11-08 14:22:32 +0100

    Include buffer size in benchmark id

--- lib/benches/bench.rs
+++ lib/benches/bench.rs
@@ -19,35 +19,37 @@ fn write_file(c: &mut Criterion) {
     let mut group = c.benchmark_group("write_file");
     group.sample_size(50);
     group.throughput(Throughput::Bytes(file_size));
-    group.bench_function(BenchmarkId::from_parameter(file_size), |b| {
-        b.to_async(&runtime).iter_custom(|iters| async move {
-            let mut elapsed = Duration::ZERO;
+    group.bench_function(
+        BenchmarkId::from_parameter(format!("{}@{}", file_size, buffer_size)),
+        |b| {
+            b.to_async(&runtime).iter_custom(|iters| async move {
+                let mut elapsed = Duration::ZERO;
 
-            for _ in 0..iters {
-                // Setup is not measured
-                // let mut rng = StdRng::seed_from_u64(0);
-                let mut rng = StdRng::from_entropy();
-                let base_dir = TempDir::new_in(env!("CARGO_TARGET_TMPDIR")).unwrap();
-                let repo = utils::create_repo(&mut rng, &base_dir.path().join("repo.db")).await;
+                for _ in 0..iters {
+                    // Setup is not measured
+                    let mut rng = StdRng::from_entropy();
+                    let base_dir = TempDir::new_in(env!("CARGO_TARGET_TMPDIR")).unwrap();
+                    let repo = utils::create_repo(&mut rng, &base_dir.path().join("repo.db")).await;
 
-                // Only this part is measured
-                let time = Instant::now();
-                utils::write_file(
-                    &mut rng,
-                    &repo,
-                    Utf8Path::new("file.dat"),
-                    file_size as usize,
-                    buffer_size,
-                )
-                .await;
-                elapsed += time.elapsed();
+                    // Only this part is measured
+                    let time = Instant::now();
+                    utils::write_file(
+                        &mut rng,
+                        &repo,
+                        Utf8Path::new("file.dat"),
+                        file_size as usize,
+                        buffer_size,
+                    )
+                    .await;
+                    elapsed += time.elapsed();
 
-                // Teardown isn't measured either
-                repo.close().await;
-            }
+                    // Teardown isn't measured either
+                    repo.close().await;
+                }
 
-            elapsed
-        })
-    });
+                elapsed
+            })
+        },
+    );
     group.finish();
 }
