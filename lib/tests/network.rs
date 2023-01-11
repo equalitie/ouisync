baa7e98e2b9b50d94ec87c903c048886c7da0f0e
Adam Cigánek <adam.ciganek@proton.me>
2023-01-11 17:02:34 +0100

    Convert more network integration tests to the new Env

--- lib/tests/network.rs
+++ lib/tests/network.rs
@@ -1,14 +1,16 @@
 //! Networking tests
 
+// These test mostly require QUIC / UDP which the simulator doesn't support yet.
+#![cfg(not(feature = "simulation"))]
+
 mod common;
 
 use self::common::{old, Env, NetworkExt, Proto, TEST_TIMEOUT};
 use ouisync::{network::Network, AccessSecrets};
 use std::{net::Ipv4Addr, sync::Arc, time::Duration};
-use tokio::{select, sync::Barrier, time};
+use tokio::{sync::Barrier, time};
 
 // This test requires QUIC which is not yet supported in simulation
-#[cfg(not(feature = "simulation"))]
 #[test]
 fn peer_exchange() {
     let mut env = Env::new();
@@ -81,37 +83,31 @@ fn network_disable_enable_idle() {
     });
 }
 
-#[tokio::test(flavor = "multi_thread")]
-async fn network_disable_enable_pending_connection() {
-    let mut env = old::Env::new();
+#[test]
+fn network_disable_enable_pending_connection() {
+    let mut env = Env::new();
     let proto = Proto::Quic;
-    let bind = proto.wrap((Ipv4Addr::LOCALHOST, 0));
 
-    let node = env.create_node(bind).await;
-    let local_addr_0 = proto.listener_local_addr_v4(&node);
+    // Just for name lookup
+    env.actor("remote", async {});
 
-    let remote_addr = proto.wrap((Ipv4Addr::LOCALHOST, 12345));
-    node.add_user_provided_peer(&remote_addr);
+    env.actor("local", async move {
+        let bind_addr = proto.wrap((Ipv4Addr::LOCALHOST, 0));
+        let network = common::create_unbound_network();
 
-    // Wait until the connection starts begin established.
-    let mut rx = node.on_peer_set_change();
-    time::timeout(TEST_TIMEOUT, async {
-        loop {
-            if node.knows_peer(remote_addr) {
-                break;
-            }
+        network.handle().bind(&[bind_addr]).await;
+        let local_addr_0 = proto.listener_local_addr_v4(&network);
 
-            rx.changed().await.unwrap();
-        }
-    })
-    .await
-    .unwrap();
+        // Connect and wait until the connection starts begin established.
+        network.connect("remote");
+        expect_knows(&network, "remote").await;
 
-    node.handle().bind(&[]).await;
-    node.handle().bind(&[bind]).await;
+        network.handle().bind(&[]).await;
+        network.handle().bind(&[bind_addr]).await;
+        let local_addr_1 = proto.listener_local_addr_v4(&network);
 
-    let local_addr_1 = proto.listener_local_addr_v4(&node);
-    assert_eq!(local_addr_1, local_addr_0);
+        assert_eq!(local_addr_1, local_addr_0);
+    });
 }
 
 #[tokio::test(flavor = "multi_thread")]
@@ -163,55 +159,36 @@ async fn dht_toggle() {
     reg.enable_dht();
 }
 
-#[tokio::test(flavor = "multi_thread")]
-async fn local_discovery() {
-    let mut env = old::Env::new();
+#[test]
+fn local_discovery() {
+    let mut env = Env::new();
     let proto = Proto::Quic;
+    let barrier = Arc::new(Barrier::new(2));
 
-    // A and B are initially disconnected and don't know each other's socket addesses.
-    let node_a = env
-        .create_node(proto.wrap((Ipv4Addr::UNSPECIFIED, 0)))
-        .await;
-    let node_b = env
-        .create_node(proto.wrap((Ipv4Addr::UNSPECIFIED, 0)))
-        .await;
+    // The peers are initially disconnected and don't know each other's socket addesses.
+    // They eventually discover each other via local discovery.
 
-    node_a.enable_local_discovery();
-    node_b.enable_local_discovery();
+    for (src_port, dst_port) in [(7001, 7002), (7002, 7001)] {
+        let barrier = barrier.clone();
 
-    // Note we compare only the ports because we bind to `UNSPECIFIED` (0.0.0.0) and that's what
-    // `listener_local_addr_v4` returns as well, but local discovery produces the actual LAN
-    // addresses. Comparing the ports should be enough to test that local discovery works.
-    let port_a = proto.listener_local_addr_v4(&node_a).port();
-    let port_b = proto.listener_local_addr_v4(&node_b).port();
+        env.actor(&format!("node-{}", src_port), async move {
+            let network = common::create_unbound_network();
+            network
+                .handle()
+                .bind(&[proto.wrap((Ipv4Addr::LOCALHOST, src_port))])
+                .await;
 
-    let mut rx_a = node_a.on_peer_set_change();
-    let mut rx_b = node_b.on_peer_set_change();
+            network.enable_local_discovery();
 
-    // ...eventually they discover each other via local discovery.
-    let connected = async {
-        loop {
-            let mut peer_ports_a = node_a
-                .collect_peer_info()
-                .into_iter()
-                .map(|info| info.addr.port());
-            let mut peer_ports_b = node_b
-                .collect_peer_info()
-                .into_iter()
-                .map(|info| info.addr.port());
+            // Note we compare only the ports because we bind to `LOCALHOST` (127.0.0.1) but local
+            // discovery produces the actual LAN addresses which we don't know in advance (or
+            // rather can't be bothered to find out). Comparing the ports should be enough to test
+            // that local discovery works.
+            expect_knows_port(&network, dst_port).await;
 
-            if peer_ports_a.any(|port| port == port_b) && peer_ports_b.any(|port| port == port_a) {
-                break;
-            }
-
-            select! {
-                result = rx_a.changed() => result.unwrap(),
-                result = rx_b.changed() => result.unwrap(),
-            }
-        }
-    };
-
-    time::timeout(TEST_TIMEOUT, connected).await.unwrap();
+            barrier.wait().await;
+        });
+    }
 }
 
 async fn expect_knows(network: &Network, peer_name: &str) {
@@ -220,6 +197,27 @@ async fn expect_knows(network: &Network, peer_name: &str) {
 
         loop {
             if network.knows(peer_name) {
+                break;
+            }
+
+            rx.changed().await.unwrap();
+        }
+    })
+    .await
+    .unwrap()
+}
+
+async fn expect_knows_port(network: &Network, peer_port: u16) {
+    time::timeout(TEST_TIMEOUT, async move {
+        let mut rx = network.on_peer_set_change();
+
+        loop {
+            let mut peer_ports = network
+                .collect_peer_info()
+                .into_iter()
+                .map(|info| info.addr.port());
+
+            if peer_ports.any(|port| port == peer_port) {
                 break;
             }
 
