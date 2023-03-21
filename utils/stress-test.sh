d5aee78d04bf28a8029b12cbea7094c0327408f3
Adam Cigánek <adam.ciganek@proton.me>
2023-03-21 09:53:27 +0100

    Improve and refactor stress-test.sh

--- utils/stress-test.sh
+++ utils/stress-test.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 function date_tag {
-    date +"%Y-%m-%d--%H-%M-%S"
+    date +"%Y-%m-%dT%H:%M:%S"
 }
 
 temp_dir_prefix="ouisync-stress-test"
 build_args="--release -p ouisync --lib"
+
+concurrency=1
 log_open=""
 log_dump=""
 
-if   [ "$1" = "--open" -o "$1" = "-o" ]; then
-    log_open="1"
-    shift 1
-elif [ "$1" = "--dump" -o "$1" = "-d" ]; then
-    log_dump="1"
-    shift 1
-elif [ "$1" = "--help" -o "$1" = "-h" ]; then
-    echo "Usage $(basename $0) [OPTIONS] <ARGS>..."
-    echo
-    echo "Options:"
-    echo "  -o, --open    Open log in \$EDITOR"
-    echo "  -d, --dump    Dump log to stdout"
-    echo
-    echo "Arguments:"
-    echo "  <ARGS>...  Arguments passed verbatim to the test executable"
+while getopts "dohn:" arg; do
+    case $arg in
+        n)
+            concurrency=$OPTARG
+            ;;
+        o)
+            log_open=1
+            ;;
+        d)
+            log_dump=1
+            ;;
+        h|*)
+            echo "Usage $(basename $0) [OPTIONS] <ARGS>..."
+            echo
+            echo "Options:"
+            echo "  -n NUMBER    Number of processes to run concurrently (default: $concurrency)"
+            echo "  -d           Dump log to stdout"
+            echo "  -o           Open log in \$EDITOR"
+            echo "  -h           Show this help"
+            echo
+            echo "Arguments:"
+            echo "  <ARGS>...  Arguments passed verbatim to the test executable"
 
-    exit -1
-fi
+            exit -1
+            ;;
+    esac
+done
+
+# Extract remaining arguments
+shift $((OPTIND-1))
+args=$@
 
 trap "pkill -P $$" EXIT
 
@@ -42,37 +57,49 @@ exe=$(cargo test $build_args --no-run 2>&1 | grep ' Executable unittests src/lib
 exe=$(echo $exe | cut -d " " -f 4 | cut -d "(" -f 2 | cut -d ")" -f 1)
 
 dir=`mktemp --tmpdir -d $temp_dir_prefix-XXXXXX`
-n=100
+
+pipe="$dir/pipe"
+mkfifo $pipe
 
 echo "$(date_tag) Working in directory $dir"
+echo "$(date_tag) Starting $concurrency tests"
 
-echo "$(date_tag) Starting $n tests"
-for i in $(seq $n); do
-    while true; do
-        RUST_BACKTRACE=full PROPTEST_CASES=32 $exe ${@:1} 2>&1
-        r=$?
-        if [ "$r" -ne "0" ]; then break; fi
-    done > $dir/test-$i.log 2>&1 &
+export RUST_BACKTRACE=full
+export PROPTEST_CASES=32
 
-    pids[${i}]=$!
+for process in $(seq $concurrency); do
+    {
+        local_iteration=0
+
+        while true; do
+            if $exe $args > $dir/test-$process.log 2>&1; then
+                ((local_iteration=local_iteration+1))
+                echo "$process $local_iteration ok" > $pipe
+            else
+                echo "$process $local_iteration fail" > $pipe
+                break
+            fi
+        done
+    } &
 done
-
-aborted_process="";
 
 echo "$(date_tag) Awaiting first process to fail"
-while [ -z "$aborted_process" ]; do
-    for i in $(seq $n); do
-        pid=${pids[${i}]}
-        if ! ps $pid > /dev/null; then
-            echo "Process $i (pid:$pid) aborted"
-            aborted_process=$i
-            break;
-        fi
-    done
-    sleep 0.5
+
+global_iteration=0
+aborted_process=""
+
+while read process local_iteration status < $pipe; do
+    if [ "$status" = "ok" ]; then
+        ((global_iteration=global_iteration+1))
+        echo "$(date_tag) Iteration #$global_iteration ($process/$local_iteration)"
+    else
+        aborted_process=$process
+        echo "$(date_tag) Process $aborted_process aborted after $local_iteration iterations"
+        break;
+    fi
 done
 
-new_log_name=/tmp/ouisync-log-$(date +"%Y-%m-%d--%H-%M-%S").txt
+new_log_name="/tmp/ouisync-log-$(date_tag).txt"
 mv $dir/test-$aborted_process.log $new_log_name
 echo "$(date_tag) Log saved to $new_log_name"
 
