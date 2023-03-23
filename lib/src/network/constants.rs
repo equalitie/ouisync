6843f3295a9a4ecc476d5660c13def42c26ab843
Peter Jankuliak <p.jankuliak@gmail.com>
2023-03-23 12:20:27 +0100

    Update network MAX_REQUESTS_IN_FLIGHT constant

--- lib/src/network/constants.rs
+++ lib/src/network/constants.rs
@@ -1,12 +1,15 @@
 // Maximum number of request which have been sent but for which we haven't received a response yet.
-// Higher values give better performance but too high risks congesting the network. Also there is a
-// point of diminishing returns. 32 seems to be the sweet spot based on a simple experiment.
-// TODO: run more precise benchmarks to find the actual optimum.
-pub(super) const MAX_REQUESTS_IN_FLIGHT: usize = 512;
+// What we want to achieve is to "saturate" the connection in the direction Server -> Client. The
+// BitTorrent Economics Paper (PDF), section 2.3 Pipelining uses the number 5 with message sizes
+// being 16KB. Our responses don't have a fixed size (blocks are bigger and nodes are smaller) so
+// calculating the max-in-flight requests is less straight forward. TODO: Maybe in the future
+// Clients can keep track of expected response sizes and limit requests in flight so responses sum
+// to 5 * 16KB = 80KB.
+pub(super) const MAX_REQUESTS_IN_FLIGHT: usize = 32;
 
 // Maximum number of respones that a `Client` received but had not yet processed before the client
 // is allowed to send more requests.
-pub(super) const MAX_PENDING_RESPONSES: usize = MAX_REQUESTS_IN_FLIGHT;
+pub(super) const MAX_PENDING_RESPONSES: usize = 512;
 
 // Maximum number of parallel tasks for request handling that the Server is going to create.
 pub(super) const MAX_SERVER_PARALLEL_HANDLING: usize = 32;
