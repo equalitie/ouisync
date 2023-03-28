2070118f6b88afffef2bb81a465f65f7be530d36
Peter Jankuliak <p.jankuliak@gmail.com>
2023-03-28 21:56:55 +0200

    Add deadlock::async::ExpectShortLifetime struct

--- lib/src/deadlock/asynch.rs
+++ lib/src/deadlock/asynch.rs
@@ -1,5 +1,10 @@
 use crate::deadlock::{DeadlockGuard, DeadlockTracker};
+use scoped_task::{spawn, ScopedJoinHandle};
 use std::future::Future;
+use std::{backtrace::Backtrace, fmt, panic::Location, time::Duration};
+use tokio::time::sleep;
+
+//------------------------------------------------------------------------------
 
 /// Replacement for `tokio::sync::Mutex` instrumented for deadlock detection.
 pub struct Mutex<T> {
@@ -25,3 +30,31 @@ impl<T> Mutex<T> {
 }
 
 pub type MutexGuard<'a, T> = DeadlockGuard<tokio::sync::MutexGuard<'a, T>>;
+
+//------------------------------------------------------------------------------
+
+pub(crate) struct ExpectShortLifetime {
+    _watcher: ScopedJoinHandle<()>,
+}
+
+impl ExpectShortLifetime {
+    #[track_caller]
+    pub fn new(max_lifetime: Duration) -> Self {
+        let file_and_line = Location::caller();
+        let backtrace = Backtrace::capture();
+        Self {
+            _watcher: spawn(async move {
+                sleep(max_lifetime.into()).await;
+                println!("Expected short lifetime, but exceeded {:?}", max_lifetime);
+                println!("Location: {:?}", file_and_line);
+                println!("Backtrace: {:?}", backtrace);
+            }),
+        }
+    }
+}
+
+impl fmt::Debug for ExpectShortLifetime {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ExpectShortLifetime")
+    }
+}
