3c1cc792c9692071233ca6e66fe1d142bf5a98c1
Peter Jankuliak <p.jankuliak@gmail.com>
2023-03-30 15:36:49 +0200

    Improve ExpectShortLifetime a bit

--- lib/src/deadlock/asynch.rs
+++ lib/src/deadlock/asynch.rs
@@ -1,7 +1,15 @@
 use crate::deadlock::{DeadlockGuard, DeadlockTracker};
 use scoped_task::{spawn, ScopedJoinHandle};
-use std::future::Future;
-use std::{fmt, panic::Location, time::Duration};
+use std::{
+    fmt,
+    future::Future,
+    panic::Location,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::{Duration, Instant},
+};
 use tokio::time::sleep;
 
 //------------------------------------------------------------------------------
@@ -33,7 +41,12 @@ pub type MutexGuard<'a, T> = DeadlockGuard<tokio::sync::MutexGuard<'a, T>>;
 
 //------------------------------------------------------------------------------
 
+static NEXT_EXPECT_SHORT_LIFETIME_ID: AtomicUsize = AtomicUsize::new(0);
+
 pub(crate) struct ExpectShortLifetime {
+    id: usize,
+    start_time: Instant,
+    shared: Arc<std::sync::Mutex<bool>>,
     _watcher: ScopedJoinHandle<()>,
 }
 
@@ -45,14 +58,49 @@ impl ExpectShortLifetime {
     }
 
     pub fn new_at(max_lifetime: Duration, file_and_line: &'static Location<'static>) -> Self {
+        let id = NEXT_EXPECT_SHORT_LIFETIME_ID.fetch_add(1, Ordering::SeqCst);
+
+        let start_time = Instant::now();
+        let shared = Arc::new(std::sync::Mutex::new(false));
+
         Self {
+            id,
+            start_time,
+            shared: shared.clone(),
             _watcher: spawn(async move {
                 sleep(max_lifetime.into()).await;
-                println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                println!("Expected short lifetime, but exceeded {:?}", max_lifetime);
-                println!("{:?}", file_and_line);
-                println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+                let mut lock = shared.lock().unwrap();
+
+                if !*lock {
+                    *lock = true;
+                    println!("ExpectShortLifetime: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    println!(
+                        "ExpectShortLifetime: Expected short lifetime, but exceeded {:?} (ID:{})",
+                        max_lifetime, id
+                    );
+                    println!("{:?}", file_and_line);
+                    println!("ExpectShortLifetime: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                }
             }),
+        }
+    }
+}
+
+impl Drop for ExpectShortLifetime {
+    fn drop(&mut self) {
+        let mut lock = self.shared.lock().unwrap();
+
+        if !*lock {
+            // The watcher did not print yet, mark it so it won't in the future.
+            *lock = true;
+        } else {
+            // The watcher printed the message.
+            println!(
+                "ExpectShortLifetime: Previously reported task with ID:{} eventually took {:?} to finish",
+                self.id,
+                self.start_time.elapsed()
+            );
         }
     }
 }
