2e62d45bc57c6dfa6398a9dd8ad95d643f4f84a1
Adam Cigánek <adam.ciganek@proton.me>
2023-05-25 11:53:12 +0200

    cli: Remove unnecessary Lazy

--- cli/src/options.rs
+++ cli/src/options.rs
@@ -1,6 +1,5 @@
 use crate::{protocol::Request, APP_NAME};
 use clap::{Args, Parser};
-use once_cell::sync::Lazy;
 use std::{env, path::PathBuf};
 
 #[derive(Parser, Debug)]
@@ -13,7 +12,7 @@ pub(crate) struct Options {
     /// bind to (if server)
     ///
     /// Can be also specified with env variable OUISYNC_SOCKET.
-    #[arg(short, long, default_value = DEFAULT_SOCKET.as_os_str(), value_name = "PATH")]
+    #[arg(short, long, default_value_os_t = default_socket(), value_name = "PATH")]
     pub socket: PathBuf,
 
     #[command(subcommand)]
@@ -25,24 +24,24 @@ pub(crate) struct Dirs {
     /// Config directory
     ///
     /// Can be also specified with env variable OUISYNC_CONFIG_DIR.
-    #[arg(long, default_value = DEFAULT_CONFIG_DIR.as_os_str(), value_name = "PATH")]
+    #[arg(long, default_value_os_t = default_config_dir(), value_name = "PATH")]
     pub config_dir: PathBuf,
 
     /// Repositories storage directory
     ///
     /// Can be also specified with env variable OUISYNC_STORE_DIR.
-    #[arg(long, default_value = DEFAULT_STORE_DIR.as_os_str(), value_name = "PATH")]
+    #[arg(long, default_value_os_t = default_store_dir(), value_name = "PATH")]
     pub store_dir: PathBuf,
 
     /// Mount directory
     ///
     /// Can be also specified with env variable OUISYNC_MOUNT_DIR.
-    #[arg(long, default_value = DEFAULT_MOUNT_DIR.as_os_str(), value_name = "PATH")]
+    #[arg(long, default_value_os_t = default_mount_dir(), value_name = "PATH")]
     pub mount_dir: PathBuf,
 }
 
 /// Path to the config directory.
-static DEFAULT_CONFIG_DIR: Lazy<PathBuf> = Lazy::new(|| {
+fn default_config_dir() -> PathBuf {
     env::var_os("OUISYNC_CONFIG_DIR")
         .map(PathBuf::from)
         .unwrap_or_else(|| {
@@ -50,9 +49,9 @@ static DEFAULT_CONFIG_DIR: Lazy<PathBuf> = Lazy::new(|| {
                 .expect("config dir not defined")
                 .join(APP_NAME)
         })
-});
+}
 
-static DEFAULT_STORE_DIR: Lazy<PathBuf> = Lazy::new(|| {
+fn default_store_dir() -> PathBuf {
     env::var_os("OUISYNC_STORE_DIR")
         .map(PathBuf::from)
         .unwrap_or_else(|| {
@@ -60,9 +59,9 @@ static DEFAULT_STORE_DIR: Lazy<PathBuf> = Lazy::new(|| {
                 .expect("data dir not defined")
                 .join(APP_NAME)
         })
-});
+}
 
-static DEFAULT_MOUNT_DIR: Lazy<PathBuf> = Lazy::new(|| {
+fn default_mount_dir() -> PathBuf {
     env::var_os("OUISYNC_MOUNT_DIR")
         .map(PathBuf::from)
         .unwrap_or_else(|| {
@@ -70,25 +69,29 @@ static DEFAULT_MOUNT_DIR: Lazy<PathBuf> = Lazy::new(|| {
                 .expect("home dir not defined")
                 .join(APP_NAME)
         })
-});
-
-static DEFAULT_SOCKET: Lazy<PathBuf> = Lazy::new(|| {
-    env::var_os("OUISYNC_SOCKET")
-        .map(PathBuf::from)
-        .unwrap_or_else(default_socket)
-});
-
-#[cfg(target_os = "linux")]
-fn default_socket() -> PathBuf {
-    // FIXME: when running as root, we should use `/run`
-    dirs::runtime_dir()
-        .or_else(dirs::cache_dir)
-        .expect("neither runtime dir nor cache dir defined")
-        .join(APP_NAME)
-        .with_extension("sock")
 }
 
-#[cfg(target_os = "windows")]
 fn default_socket() -> PathBuf {
-    format!(r"\\.\pipe\{APP_NAME}").into()
+    env::var_os("OUISYNC_SOCKET")
+        .map(PathBuf::from)
+        .unwrap_or_else(platform::default_socket)
+}
+
+mod platform {
+    use super::*;
+
+    #[cfg(target_os = "linux")]
+    pub(super) fn default_socket() -> PathBuf {
+        // FIXME: when running as root, we should use `/run`
+        dirs::runtime_dir()
+            .or_else(dirs::cache_dir)
+            .expect("neither runtime dir nor cache dir defined")
+            .join(APP_NAME)
+            .with_extension("sock")
+    }
+
+    #[cfg(target_os = "windows")]
+    pub(super) fn default_socket() -> PathBuf {
+        format!(r"\\.\pipe\{APP_NAME}").into()
+    }
 }
