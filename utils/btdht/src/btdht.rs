56e66e2a594e1c282266ef52dd14c7b155b4b1eb
Peter Jankuliak <p.jankuliak@gmail.com>
2023-07-11 08:27:32 +0200

    Modify utils/btdht to be able to send single messages to individual nodes

--- utils/btdht/src/btdht.rs
+++ utils/btdht/src/btdht.rs
@@ -7,15 +7,23 @@ use ouisync_lib::{
 use std::{
     collections::HashSet,
     io,
-    net::{Ipv4Addr, Ipv6Addr},
+    net::{Ipv4Addr, Ipv6Addr, SocketAddr},
 };
 use structopt::StructOpt;
 use tokio::{net::UdpSocket, task};
 
 #[derive(StructOpt, Debug)]
+enum Single {
+    Lookup { peer: SocketAddr },
+    Announce { peer: SocketAddr },
+}
+
+#[derive(StructOpt, Debug)]
 enum Action {
     Lookup,
     Announce,
+    // Just send a request to a single node, no need to bootstrap
+    Single(Single),
 }
 
 /// Command line options.
@@ -37,6 +45,19 @@ async fn main() -> io::Result<()> {
     let options = Options::from_args();
 
     env_logger::init();
+
+    let info_hash = if let Some(token) = &options.token {
+        Some(network::repository_info_hash(token.id()))
+    } else if let Some(swarm_name) = &options.swarm_name {
+        Some(InfoHash::sha1(swarm_name.as_bytes()))
+    } else {
+        None
+    };
+
+    if let Action::Single(single_action) = &options.action {
+        run_single_action(info_hash.unwrap(), single_action).await;
+        return Ok(());
+    }
 
     const WITH_IPV4: bool = true;
     const WITH_IPV6: bool = true;
@@ -74,28 +95,24 @@ async fn main() -> io::Result<()> {
             .unwrap()
     });
 
+    let announce = match options.action {
+        Action::Lookup => false,
+        Action::Announce => true,
+        Action::Single(action) => match action {
+            Single::Lookup { .. } => false,
+            Single::Announce { .. } => true,
+        },
+    };
+
     if options.token.is_some() && options.swarm_name.is_some() {
         println!("Only one of the token, swarm_name options may be set");
         return Ok(());
     }
 
-    let info_hash = if let Some(token) = options.token {
-        Some(network::repository_info_hash(token.id()))
-    } else if let Some(swarm_name) = options.swarm_name {
-        Some(InfoHash::sha1(swarm_name.as_bytes()))
-    } else {
-        None
-    };
-
     if let Some(info_hash) = info_hash {
         println!("InfoHash: {info_hash:?}");
 
         let task = task::spawn(async move {
-            let announce = match options.action {
-                Action::Lookup => false,
-                Action::Announce => true,
-            };
-
             if let Some(dht) = dht_v4 {
                 println!();
                 lookup("IPv4", &dht, info_hash, announce).await;
@@ -114,6 +131,78 @@ async fn main() -> io::Result<()> {
     }
 
     Ok(())
+}
+
+async fn run_single_action(info_hash: InfoHash, command: &Single) {
+    use btdht::message::{AnnouncePeerRequest, GetPeersRequest, Message, MessageBody, Request};
+    use std::time::Duration;
+    use tokio::time::timeout;
+
+    let (node_addr, announce) = match command {
+        Single::Lookup { peer } => (peer, false),
+        Single::Announce { peer } => (peer, true),
+    };
+
+    let socket = match node_addr {
+        SocketAddr::V4(_) => UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).await.unwrap(),
+        SocketAddr::V6(_) => UdpSocket::bind((Ipv6Addr::UNSPECIFIED, 0)).await.unwrap(),
+    };
+
+    let get_peers = Message {
+        transaction_id: Vec::from(rand::random::<[u8; 4]>()),
+        body: MessageBody::Request(Request::GetPeers(GetPeersRequest {
+            id: rand::random(),
+            info_hash,
+            want: None,
+        })),
+    };
+
+    println!("Sending GetPeers request to {node_addr:?}");
+    socket
+        .send_to(&get_peers.encode(), node_addr)
+        .await
+        .unwrap();
+
+    let response = loop {
+        let mut buffer = vec![0u8; 1500];
+
+        let (size, _from) = timeout(Duration::from_secs(5), socket.recv_from(&mut buffer))
+            .await
+            .unwrap()
+            .unwrap();
+
+        buffer.truncate(size);
+        let response = Message::decode(&buffer).unwrap();
+        if response.transaction_id == get_peers.transaction_id {
+            match response.body {
+                MessageBody::Request(_) => {
+                    println!("Received a request to request");
+                }
+                MessageBody::Response(response) => {
+                    println!("Received {response:?}");
+                    break response;
+                }
+                MessageBody::Error(error) => {
+                    println!("Received error response: {error:?}");
+                }
+            }
+            return;
+        }
+    };
+
+    if announce {
+        let announce = Message {
+            transaction_id: Vec::from(rand::random::<[u8; 4]>()),
+            body: MessageBody::Request(Request::AnnouncePeer(AnnouncePeerRequest {
+                id: rand::random(),
+                info_hash,
+                token: response.token.clone().unwrap(),
+                port: None,
+            })),
+        };
+
+        socket.send_to(&announce.encode(), node_addr).await.unwrap();
+    }
 }
 
 async fn lookup(prefix: &str, dht: &MainlineDht, info_hash: InfoHash, announce: bool) {
