0e6b1c3145667578e57677c92f5cdbd57f396052
Peter Jankuliak <p.jankuliak@gmail.com>
2023-09-05 14:56:41 +0200

    Add `broadcast_hash_set` channel

--- lib/src/sync.rs
+++ lib/src/sync.rs
@@ -72,6 +72,7 @@ pub mod broadcast {
 pub(crate) mod uninitialized_watch {
     use futures_util::{stream, Stream};
     use tokio::sync::watch as w;
+    pub use w::error::RecvError;
 
     pub struct Sender<T>(w::Sender<Option<T>>);
 
@@ -210,5 +211,114 @@ pub(crate) mod atomic_slot {
         fn deref(&self) -> &Self::Target {
             self.0.deref()
         }
+    }
+}
+
+/// Similar to `tokio::sync::broadcast` but does not enqueue the values. Instead, it "accumulates"
+/// them into a set. Advantages include that one doesn't need to specify the recv buffer size and
+/// the `insert` (analogue to `broadcast::send`) is non-async and never fails.
+pub(crate) mod broadcast_hash_set {
+    use super::uninitialized_watch;
+    pub use super::uninitialized_watch::RecvError;
+    use std::{
+        collections::{HashMap, HashSet},
+        hash::Hash,
+        sync::{
+            atomic::{AtomicUsize, Ordering},
+            Arc, Mutex,
+        },
+    };
+
+    #[derive(Clone)]
+    pub struct Sender<T> {
+        shared: Arc<Shared<T>>,
+    }
+
+    impl<T: Eq + Hash + Clone> Sender<T> {
+        pub fn insert(&self, value: &T) {
+            let mut receivers = self.shared.receivers.lock().unwrap();
+
+            for (_id, receiver) in receivers.iter_mut() {
+                if receiver.1.insert(value.clone()) {
+                    receiver.0.send(()).unwrap_or(());
+                }
+            }
+        }
+
+        pub fn subscribe(&self) -> Receiver<T> {
+            let id = self.shared.id_generator.fetch_add(1, Ordering::Relaxed);
+            let (watch_tx, watch_rx) = uninitialized_watch::channel();
+
+            let mut receivers = self.shared.receivers.lock().unwrap();
+
+            receivers.insert(id, (watch_tx, HashSet::new()));
+
+            Receiver {
+                id,
+                shared: self.shared.clone(),
+                watch_rx,
+            }
+        }
+    }
+
+    pub struct Receiver<T> {
+        id: usize,
+        shared: Arc<Shared<T>>,
+        watch_rx: uninitialized_watch::Receiver<()>,
+    }
+
+    impl<T> Receiver<T> {
+        pub async fn changed(&mut self) -> Result<HashSet<T>, RecvError> {
+            self.watch_rx.changed().await?;
+
+            let mut receivers = self.shared.receivers.lock().unwrap();
+
+            // Unwrap is OK because the entry must exists for as long as this `Receiver` exists.
+            let old_set = &mut receivers.get_mut(&self.id).unwrap().1;
+            let mut new_set = HashSet::new();
+
+            if !old_set.is_empty() {
+                std::mem::swap(&mut new_set, old_set);
+            }
+
+            Ok(new_set)
+        }
+    }
+
+    impl<T> Drop for Receiver<T> {
+        fn drop(&mut self) {
+            self.shared.receivers.lock().unwrap().remove(&self.id);
+        }
+    }
+
+    struct Shared<T> {
+        id_generator: AtomicUsize,
+        receivers: Mutex<HashMap<usize, (uninitialized_watch::Sender<()>, HashSet<T>)>>,
+    }
+
+    pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+        let id_generator = AtomicUsize::new(0);
+        let mut receivers = HashMap::new();
+
+        let id = id_generator.fetch_add(1, Ordering::Relaxed);
+        let (watch_tx, watch_rx) = uninitialized_watch::channel();
+
+        receivers.insert(id, (watch_tx, HashSet::new()));
+
+        let shared = Arc::new(Shared {
+            id_generator,
+            receivers: Mutex::new(receivers),
+        });
+
+        (
+            Sender {
+                shared: shared.clone(),
+            },
+            Receiver {
+                id,
+                shared,
+                watch_rx,
+            },
+        )
     }
 }
