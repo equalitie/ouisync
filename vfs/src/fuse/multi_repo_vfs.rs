e5ffcc37004f72c2a95e8ff89017436ffcbe339c
Adam Cigánek <adam.ciganek@proton.me>
2023-09-06 12:41:13 +0200

    vfs: rmdir mount points after unmount

--- vfs/src/fuse/multi_repo_vfs.rs
+++ vfs/src/fuse/multi_repo_vfs.rs
@@ -3,6 +3,7 @@ use crate::{MountError, MultiRepoMount};
 use ouisync_lib::Repository;
 use std::{
     collections::HashMap,
+    ffi::OsStr,
     fs,
     future::{self, Future},
     io,
@@ -15,7 +16,7 @@ use tokio::runtime::Handle as RuntimeHandle;
 pub struct MultiRepoVFS {
     runtime_handle: RuntimeHandle,
     mount_point: PathBuf,
-    repositories: Mutex<HashMap<PathBuf, MountGuard>>,
+    repositories: Mutex<HashMap<PathBuf, Mount>>,
 }
 
 impl MultiRepoMount for MultiRepoVFS {
@@ -30,25 +31,20 @@ impl MultiRepoMount for MultiRepoVFS {
     }
 
     fn insert(&self, store_path: PathBuf, repo: Arc<Repository>) -> Result<(), io::Error> {
-        let name = store_path.file_stem().ok_or_else(|| {
-            io::Error::new(
-                // InvalidFilename would have been better, but it's unstable.
-                io::ErrorKind::InvalidInput,
-                format!("invalid repository path: {:?}", store_path),
-            )
-        })?;
+        let mount_point = extract_mount_point(&store_path)?;
+        let mount_point = self.mount_point.join(mount_point);
 
-        let mount_point = self.mount_point.join(name);
-
-        // TODO: this should be async
+        // TODO: should this be async?
         fs::create_dir_all(&mount_point)?;
 
-        let mount_guard = super::mount(self.runtime_handle.clone(), repo, mount_point)?;
+        let mount_guard = super::mount(self.runtime_handle.clone(), repo, &mount_point)?;
 
-        self.repositories
-            .lock()
-            .unwrap()
-            .insert(store_path, mount_guard);
+        let mount = Mount {
+            point: mount_point,
+            guard: Some(mount_guard),
+        };
+
+        self.repositories.lock().unwrap().insert(store_path, mount);
 
         Ok(())
     }
@@ -57,4 +53,30 @@ impl MultiRepoMount for MultiRepoVFS {
         self.repositories.lock().unwrap().remove(store_path);
         Ok(())
     }
+}
+
+// Wrapper for `MountGuard` which also removes the mount directory after unmount on drop.
+struct Mount {
+    point: PathBuf,
+    guard: Option<MountGuard>,
+}
+
+impl Drop for Mount {
+    fn drop(&mut self) {
+        self.guard.take();
+
+        if let Err(error) = fs::remove_dir(&self.point) {
+            tracing::error!(?error, path = ?self.point, "Failed to remove mount point");
+        }
+    }
+}
+
+fn extract_mount_point(store_path: &Path) -> Result<&OsStr, io::Error> {
+    store_path.file_stem().ok_or_else(|| {
+        io::Error::new(
+            // InvalidFilename would have been better, but it's unstable.
+            io::ErrorKind::InvalidInput,
+            format!("invalid repository path: {:?}", store_path),
+        )
+    })
 }
