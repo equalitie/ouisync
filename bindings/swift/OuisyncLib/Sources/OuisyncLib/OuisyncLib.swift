dd29f411b0fdfabd6426e9125569b227f024b90d
Peter Jankuliak <inetic@gmail.com>
2024-04-09 08:53:51 +0200

    swift: add subscibeToRepositoryListChange function

--- bindings/swift/OuisyncLib/Sources/OuisyncLib/OuisyncLib.swift
+++ bindings/swift/OuisyncLib/Sources/OuisyncLib/OuisyncLib.swift
@@ -11,21 +11,45 @@ public func listRepositories(_ messageId: MessageId) -> [UInt8] {
     return makeRequest(messageId, "list_repositories", MessagePackValue.nil)
 }
 
-func makeRequest(_ messageId: MessageId, _ function_name: String, _ arguments: MessagePackValue) -> [UInt8] {
-    var message: [UInt8] = []
-    message.append(contentsOf: withUnsafeBytes(of: messageId.bigEndian, Array.init))
-    let m = [MessagePackValue.string(function_name): arguments]
-    message.append(contentsOf: pack(MessagePackValue.map(m)))
-    return message
+public func subscribeToRepositoryListChange(_ messageId: MessageId) -> [UInt8] {
+    return makeRequest(messageId, "list_repositories_subscribe", MessagePackValue.nil)
 }
 
-public enum Response {
+public class Response {
+    public let messageId: MessageId
+    public let payload: ResponsePayload
+
+    init(_ messageId: MessageId, _ payload: ResponsePayload) {
+        self.messageId = messageId
+        self.payload = payload
+    }
+}
+
+extension Response: CustomStringConvertible {
+    public var description: String {
+        return "response(\(messageId), \(payload))"
+    }
+}
+
+public enum ResponsePayload {
     case success(MessagePackValue)
     case error(ErrorCode, String)
     case notification(MessagePackValue)
 }
 
-public func parseResponse(_ data: [UInt8]) -> (MessageId, Response)? {
+extension ResponsePayload: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .success(let value):
+            return "success(\(value))"
+        case .error(let errorCode, let message):
+            return "error(\(errorCode), \(message))"
+        case .notification(let value):          return "notification(\(value))"
+        }
+    }
+}
+
+public func parseResponse(_ data: [UInt8]) -> Response? {
     let idByteCount = (MessageId.bitWidth / UInt8.bitWidth)
 
     if data.count < idByteCount {
@@ -33,33 +57,38 @@ public func parseResponse(_ data: [UInt8]) -> (MessageId, Response)? {
     }
 
     let bigEndianValue = data.withUnsafeBufferPointer {
-             ($0.baseAddress!.withMemoryRebound(to: MessageId.self, capacity: 1) { $0 })
+        ($0.baseAddress!.withMemoryRebound(to: MessageId.self, capacity: 1) { $0 })
     }.pointee
-    
+
     let id = MessageId(bigEndian: bigEndianValue)
-    
+
     let unpacked = (try? unpack(Data(data[idByteCount...])))?.0
-    NSLog("ZZZZZZZZZZZZZZZ \(unpacked)")
+
     if case let .map(m) = unpacked {
         if let success = m[.string("success")] {
-            let optional_value = parseSuccess(success)
-            if let value = optional_value {
-                return (id, Response.success(value))
+            if let value = parseSuccess(success) {
+                return Response(id, ResponsePayload.success(value))
             }
         } else if let error = m[.string("failure")] {
-            let optional_value = parseFailure(error)
-            if let (ec, message) = optional_value {
-                return (id, Response.error(ec, message))
+            if let (ec, message) = parseFailure(error) {
+                return Response(id, ResponsePayload.error(ec, message))
             }
         } else if let notification = m[.string("notification")] {
-            let optional_value = parseNotification(notification)
-            if let value = optional_value {
-                return (id, Response.notification(value))
+            if let value = parseNotification(notification) {
+                return Response(id, ResponsePayload.notification(value))
             }
         }
     }
 
     return nil
+}
+
+func makeRequest(_ messageId: MessageId, _ function_name: String, _ arguments: MessagePackValue) -> [UInt8] {
+    var message: [UInt8] = []
+    message.append(contentsOf: withUnsafeBytes(of: messageId.bigEndian, Array.init))
+    let m = [MessagePackValue.string(function_name): arguments]
+    message.append(contentsOf: pack(MessagePackValue.map(m)))
+    return message
 }
 
 func parseSuccess(_ value: MessagePackValue) -> MessagePackValue? {
@@ -86,7 +115,12 @@ func parseFailure(_ value: MessagePackValue) -> (ErrorCode, String)? {
     return nil
 }
 
+/// Note that returning `nil` means parsing failed, but returning `MessagePackValue.nil` is a success
+/// but the message had no payload.
 func parseNotification(_ value: MessagePackValue) -> MessagePackValue? {
+    if case .string(_) = value {
+        return MessagePackValue.nil
+    }
     if case let .map(m) = value {
         if m.count != 1 {
             return nil
