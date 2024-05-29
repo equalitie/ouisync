4ffe12add39abe7a5bd15145e0eda6b8c7564858
Peter Jankuliak <inetic@gmail.com>
2024-05-29 17:39:31 +0200

    Change .swift file headers to establish MPL2 license

--- bindings/swift/OuisyncLib/Sources/OuisyncLib/OuisyncLib.swift
+++ bindings/swift/OuisyncLib/Sources/OuisyncLib/OuisyncLib.swift
@@ -1,9 +1,10 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 import Foundation
 
 public typealias MessageId = UInt64
 public typealias RepositoryHandle = UInt64
-//public typealias ErrorCode = UInt64
-
