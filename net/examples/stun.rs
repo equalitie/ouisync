b38d8e5fc04b4e96dd40c7454faf365edd0bbea7
Peter Jankuliak <p.jankuliak@gmail.com>
2024-10-18 17:33:16 +0200

    Stun example: continue after error

--- net/examples/stun.rs
+++ net/examples/stun.rs
@@ -18,8 +18,8 @@ async fn main() -> io::Result<()> {
     )?);
 
     for server_name in options.servers {
-        for server_addr in net::lookup_host(server_name).await? {
-            println!("STUN server {server_addr}:");
+        for server_addr in net::lookup_host(server_name.clone()).await? {
+            println!("STUN server {server_name} ({server_addr}):");
 
             let client = match server_addr {
                 SocketAddr::V4(_) => &client_v4,
@@ -27,13 +27,13 @@ async fn main() -> io::Result<()> {
             };
 
             let result = client.external_addr(server_addr).await;
-            println!("  external address:       {result:?}");
+            println!("  external address:       {}", format_display(result));
 
-            let result = client.nat_filtering(server_addr).await?;
-            println!("  NAT filtering behavior: {result:?}");
+            let result = client.nat_filtering(server_addr).await;
+            println!("  NAT filtering behavior: {}", format_debug(result));
 
-            let result = client.nat_mapping(server_addr).await?;
-            println!("  NAT mapping behavior:   {result:?}");
+            let result = client.nat_mapping(server_addr).await;
+            println!("  NAT mapping behavior:   {}", format_debug(result));
 
             println!();
         }
@@ -49,4 +49,18 @@ struct Options {
 
     #[arg(short, long, default_value = "stun1.l.google.com:19305")]
     servers: Vec<String>,
+}
+
+fn format_debug<T: std::fmt::Debug>(result: io::Result<T>) -> String {
+    match result {
+        Ok(value) => format!("{value:?}"),
+        Err(err) => format!("Error: {err}"),
+    }
+}
+
+fn format_display<T: std::fmt::Display>(result: io::Result<T>) -> String {
+    match result {
+        Ok(value) => format!("{value}"),
+        Err(err) => format!("Error: {err}"),
+    }
 }
