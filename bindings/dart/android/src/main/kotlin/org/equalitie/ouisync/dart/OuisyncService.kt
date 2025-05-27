168e75839191d38976ce87e6b3294500a9211050
Adam Cigánek <adam.ciganek@proton.me>
2025-05-27 16:22:06 +0200

    bindings/dart: Launch the app on notification tap

--- bindings/dart/android/src/main/kotlin/org/equalitie/ouisync/dart/OuisyncService.kt
+++ bindings/dart/android/src/main/kotlin/org/equalitie/ouisync/dart/OuisyncService.kt
@@ -3,13 +3,16 @@ package org.equalitie.ouisync.dart
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.pm.ServiceInfo
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import kotlin.collections.firstOrNull
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -123,24 +126,72 @@ class OuisyncService : Service() {
     }
 
     protected open fun createNotification(): Notification =
-        // TODO: bring the app to the foreground when the notification is tapped
         Notification
             .Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.notification_icon)
             .setContentTitle(notificationContentTitle)
+            .setOngoing(true)
+            .setPriority(Notification.PRIORITY_LOW)
+            .setCategory(Notification.CATEGORY_SERVICE)
             .apply {
                 val text = notificationContentText
                 if (text != null) {
                     setContentText(text)
                 }
-            }.setOngoing(true)
-            .setPriority(Notification.PRIORITY_LOW)
-            .setCategory(Notification.CATEGORY_SERVICE)
+
+                val intent = createContentIntent()
+                if (intent != null) {
+                    setContentIntent(intent)
+                }
+            }
             .build()
 
     protected open val notificationChannelName: String = "Ouisync notification channel"
     protected open val notificationContentTitle: String = "Ouisync is running"
     protected open val notificationContentText: String? = null
+
+    private fun createContentIntent(): PendingIntent? {
+        val activityClass = getMainActivityClass()
+        if (activityClass == null) {
+            return null
+        }
+
+        val intent = Intent(this, activityClass).apply {
+            setAction(Intent.ACTION_MAIN)
+            addCategory(Intent.CATEGORY_LAUNCHER)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+
+        val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            PendingIntent.FLAG_IMMUTABLE
+        } else {
+            0
+        }
+
+        return PendingIntent.getActivity(this, 0, intent, flags)
+    }
+
+    // Returns the class of the "main" activity of the current application. This assumes there is
+    // only one launchable activity in the app. If that's not the case, it arbitrarily returns one
+    // such activity which might not be what you want. In such cases it's recommented to override
+    // this method.
+    protected open fun getMainActivityClass(): Class<*>? = applicationContext.let { context ->
+        context.packageManager
+            .getPackageInfo(context.packageName, PackageManager.GET_ACTIVITIES)
+            .activities
+            .asSequence()
+            .map { info -> Class.forName(info.name) }
+            .filter { klass ->
+                val intent = Intent(context, klass).apply {
+                    addCategory(Intent.CATEGORY_LAUNCHER)
+                }
+
+                context
+                    .packageManager
+                    .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+                    .isNotEmpty()
+            }.firstOrNull()
+    }
 
     companion object {
         const val EXTRA_CONFIG_PATH = "config-path"
