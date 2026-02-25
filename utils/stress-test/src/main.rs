1e75847b25a8920655f84537a5c9f37ed3cf6146
Adam Cigánek <adam.ciganek@proton.me>
2026-02-25 13:46:46 +0100

    utils/stress-test: Report slow processes

--- utils/stress-test/src/main.rs
+++ utils/stress-test/src/main.rs
@@ -3,10 +3,11 @@ use serde::Deserialize;
 use std::{
     borrow::Cow,
     env, fmt,
-    io::{self, BufRead, BufReader},
-    process::{self, Command, Output, Stdio},
-    sync::mpsc,
-    thread,
+    io::{self, BufRead, BufReader, Read, Write},
+    mem,
+    process::{self, Child, ChildStderr, ChildStdout, Command, Stdio},
+    sync::mpsc::{self, RecvTimeoutError},
+    thread::{self},
     time::{Duration, Instant},
 };
 use tempfile::TempDir;
@@ -78,27 +79,51 @@ fn main() {
 
     let start = Instant::now();
 
-    for (global_iteration, status) in rx.into_iter().enumerate() {
+    for (global_iteration, progress) in rx.into_iter().enumerate() {
         print!(
             "{} #{} ({}/{})",
             DisplayDuration(start.elapsed()),
             global_iteration,
-            status.process,
-            status.iteration,
+            progress.process,
+            progress.iteration,
         );
 
-        match status.result {
-            Ok(()) => println!(),
-            Err(output) => {
-                println!("\tFAILURE ({})", output.status);
+        match progress.status {
+            Status::Success => println!(),
+            Status::Failure {
+                stdout,
+                stderr,
+                code,
+            } => {
+                print!("\tFAILURE (");
+                if let Some(code) = code {
+                    print!("exit code: {code}");
+                } else {
+                    print!("terminated by signal")
+                }
+                println!(")");
 
                 println!("\n\n---- stdout: ----\n\n");
-                io::copy(&mut &output.stdout[..], &mut io::stdout()).unwrap();
+                io::stdout().write_all(&stdout).unwrap();
 
                 println!("\n\n---- stderr: ----\n\n");
-                io::copy(&mut &output.stderr[..], &mut io::stdout()).unwrap();
+                io::stderr().write_all(&stderr).unwrap();
 
                 break;
+            }
+            Status::Slow {
+                elapsed,
+                stdout,
+                stderr,
+            } => {
+                println!("\ttaking too long (so far: {})", DisplayDuration(elapsed));
+                println!("\noutput since last report:");
+
+                println!("\n\n---- stdout: ----\n\n");
+                io::stdout().write_all(&stdout).unwrap();
+
+                println!("\n\n---- stderr: ----\n\n");
+                io::stderr().write_all(&stderr).unwrap();
             }
         }
     }
@@ -249,7 +274,7 @@ fn run(
     runner: Option<String>,
     exes: Vec<String>,
     args: Vec<String>,
-    tx: mpsc::SyncSender<Status>,
+    tx: mpsc::SyncSender<Progress>,
 ) {
     let mut commands: Vec<_> = exes
         .into_iter()
@@ -267,35 +292,57 @@ fn run(
         })
         .collect();
 
+    let runner = CommandRunner::new();
+
     for iteration in 0.. {
-        let result = commands
-            .iter_mut()
-            .map(|command| command.output().unwrap())
-            .find(|output| !output.status.success())
-            .map(Err)
-            .unwrap_or(Ok(()));
-        let is_failure = result.is_err();
+        for command in &mut commands {
+            let mut running = runner.run(command);
 
-        let status = Status {
-            process,
-            iteration,
-            result,
-        };
-
-        if tx.send(status).is_err() {
-            break;
+            loop {
+                match running.next(Duration::from_mins(1)) {
+                    Status::Success => break,
+                    status @ Status::Failure { .. } => {
+                        tx.send(Progress {
+                            process,
+                            iteration,
+                            status,
+                        })
+                        .ok();
+                        return;
+                    }
+                    status @ Status::Slow { .. } => {
+                        if tx
+                            .send(Progress {
+                                process,
+                                iteration,
+                                status,
+                            })
+                            .is_err()
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
         }
 
-        if is_failure {
+        if tx
+            .send(Progress {
+                process,
+                iteration,
+                status: Status::Success,
+            })
+            .is_err()
+        {
             break;
         }
     }
 }
 
-struct Status {
+struct Progress {
     process: u64,
     iteration: u64,
-    result: Result<(), Output>,
+    status: Status,
 }
 
 struct DisplayDuration(Duration);
@@ -329,4 +376,145 @@ fn get_runner_from_env() -> Option<String> {
         // TODO: other targets
         _ => None,
     }
+}
+
+struct CommandRunner {
+    stdout_tx: mpsc::SyncSender<(ChildStdout, mpsc::SyncSender<Chunk>)>,
+    stderr_tx: mpsc::SyncSender<(ChildStderr, mpsc::SyncSender<Chunk>)>,
+}
+
+impl CommandRunner {
+    fn new() -> Self {
+        fn forward_output<R: Read>(
+            buffer: Buffer,
+            rx: mpsc::Receiver<(R, mpsc::SyncSender<Chunk>)>,
+        ) {
+            let mut chunk = [0; 1024];
+            for (mut reader, status_tx) in rx {
+                loop {
+                    match reader.read(&mut chunk) {
+                        Ok(n @ 1..) => {
+                            status_tx.send((buffer, chunk[..n].to_vec())).ok();
+                        }
+                        Ok(0) => break,
+                        Err(error) => {
+                            eprintln!("failed to read from process output: {error:?}");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        let (stdout_tx, stdout_rx) = mpsc::sync_channel(1);
+        thread::spawn(move || forward_output(Buffer::Stdout, stdout_rx));
+
+        let (stderr_tx, stderr_rx) = mpsc::sync_channel(1);
+        thread::spawn(move || forward_output(Buffer::Stderr, stderr_rx));
+
+        Self {
+            stdout_tx,
+            stderr_tx,
+        }
+    }
+
+    fn run(&self, command: &mut Command) -> Running {
+        let mut child = command
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap();
+        let stdout = child.stdout.take().unwrap();
+        let stderr = child.stderr.take().unwrap();
+
+        let (output_tx, output_rx) = mpsc::sync_channel(1);
+
+        self.stdout_tx.send((stdout, output_tx.clone())).ok();
+        self.stderr_tx.send((stderr, output_tx)).ok();
+
+        Running {
+            child,
+            output_rx,
+            start: Instant::now(),
+            stdout: Vec::new(),
+            stdout_offset: 0,
+            stderr: Vec::new(),
+            stderr_offset: 0,
+        }
+    }
+}
+
+struct Running {
+    child: Child,
+    output_rx: mpsc::Receiver<Chunk>,
+    start: Instant,
+    stdout: Vec<u8>,
+    stdout_offset: usize,
+    stderr: Vec<u8>,
+    stderr_offset: usize,
+}
+
+impl Running {
+    fn next(&mut self, timeout: Duration) -> Status {
+        loop {
+            match self.output_rx.recv_timeout(timeout) {
+                Ok((buffer, mut chunk)) => match buffer {
+                    Buffer::Stdout => {
+                        self.stdout.append(&mut chunk);
+                    }
+                    Buffer::Stderr => {
+                        self.stderr.append(&mut chunk);
+                    }
+                },
+                Err(RecvTimeoutError::Timeout) => {
+                    let stdout = self.stdout[self.stdout_offset..].to_vec();
+                    self.stdout_offset = self.stdout.len();
+
+                    let stderr = self.stderr[self.stderr_offset..].to_vec();
+                    self.stderr_offset = self.stderr.len();
+
+                    break Status::Slow {
+                        elapsed: self.start.elapsed(),
+                        stdout,
+                        stderr,
+                    };
+                }
+                Err(RecvTimeoutError::Disconnected) => {
+                    let status = self.child.wait().unwrap();
+
+                    if status.success() {
+                        break Status::Success;
+                    } else {
+                        break Status::Failure {
+                            stdout: mem::take(&mut self.stdout),
+                            stderr: mem::take(&mut self.stderr),
+                            code: status.code(),
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Buffer {
+    Stdout,
+    Stderr,
+}
+
+type Chunk = (Buffer, Vec<u8>);
+
+enum Status {
+    Success,
+    Failure {
+        stdout: Vec<u8>,
+        stderr: Vec<u8>,
+        code: Option<i32>,
+    },
+    Slow {
+        elapsed: Duration,
+        stdout: Vec<u8>,
+        stderr: Vec<u8>,
+    },
 }
