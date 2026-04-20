e29e2d97effea8d448bce8d43691ba7e40d70df3
Adam Cigánek <adam.ciganek@proton.me>
2026-04-20 09:38:26 +0200

    bindings/cpp: Improve the cancellation test

--- bindings/cpp/tests/test_cancellation.cpp
+++ bindings/cpp/tests/test_cancellation.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE dht
+#define BOOST_TEST_MODULE cancellation
 #include <boost/test/included/unit_test.hpp>
 
 #include <boost/asio.hpp>
@@ -23,31 +23,29 @@ BOOST_AUTO_TEST_CASE(test_cancellation) {
         service.start(config_dir, nullptr, yield);
 
         auto session = ouisync::Session::connect(config_dir, yield);
-        session.set_store_dirs({ mkdir(tempdir.path() / "store").string() }, yield);
+        session.bind_network({"quic/127.0.0.1:0"}, yield);
+        auto socket = session.open_network_socket_v4(yield);
 
-        // invoke the operation without waiting for its result immediatelly. Use this channel to
+        // Invoke the operation without waiting for its result immediatelly but use this channel to
         // retrieve the result later.
-        asio::experimental::channel<void(boost::system::error_code, ouisync::Repository)> channel(ctx, 0);
+        asio::experimental::channel<void(boost::system::error_code, ouisync::Datagram)> channel(ctx, 0);
 
         asio::cancellation_signal cancellation_signal;
         auto cancellation_slot = cancellation_signal.slot();
 
-        session.create_repository(
-            "my-repo",
-            std::nullopt,
-            std::nullopt,
-            std::nullopt,
-            false,
-            false,
-            false,
+        // Try to receive data on the raw UDP socket. No one is sending anything to us at this point
+        // so this should wait indefinitely.
+        socket.recv_from(
+            1024,
             asio::bind_cancellation_slot(
                 cancellation_slot,
-                [&](boost::system::error_code ec, ouisync::Repository repo) {
-                    channel.async_send(ec, std::move(repo), asio::detached);
+                [&](boost::system::error_code ec, ouisync::Datagram datagram) {
+                    channel.async_send(ec, std::move(datagram), asio::detached);
                 }
             )
         );
 
+        // The operation should still be ongoing. Cancel it.
         cancellation_signal.emit(asio::cancellation_type::total);
 
         boost::system::error_code ec;
